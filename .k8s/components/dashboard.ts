36af8db09179341ade62e230cbba204a89469538
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2021-09-10 11:39:02 +0200

    feat(ci): add component dashboard only for dev env (#9)
    * feat(ci): add components
    
    * chore: update dockerfile
    
    * chore: update workflow
    
    * chore: oops

---
+++ .k8s/components/dashboard.ts
@@ -0,0 +1,31 @@
+import env from "@kosko/env";
+import { create } from "@socialgouv/kosko-charts/components/app";
+
+const tag = process.env.GITHUB_SHA;
+
+const manifests = create("dashboard", {
+  env,
+  config: {
+    containerPort: 80,
+    subDomainPrefix: "dashboard-",
+    image: `ghcr.io/socialgouv/mano/dashboard:sha-${tag}`,
+  },
+  deployment: {
+    // private registry need a registry secret
+    imagePullSecrets: [{ name: "regcred" }],
+    container: {
+      resources: {
+        requests: {
+          cpu: "10m",
+          memory: "64Mi",
+        },
+        limits: {
+          cpu: "200m",
+          memory: "128Mi",
+        },
+      },
+    },
+  },
+});
+
+export default manifests;
