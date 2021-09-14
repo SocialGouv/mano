d70b1e5b2e3c9bd06ab56e4741e78f40d03bf947
Raphaël Huchet <raph@selego.co>
2021-09-14 14:46:48 +0200

    feat: add metabase

---
+++ .k8s/components/metabase.ts
@@ -0,0 +1,8 @@
+import env from "@kosko/env";
+import { create } from "@socialgouv/kosko-charts/components/metabase";
+
+const manifests = create("metabase", {
+  env,
+});
+
+export default manifests;
