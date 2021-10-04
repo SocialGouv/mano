dfd7884bebad0d3103c14ffc838ccadda43419f6
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2021-10-04 14:59:18 +0200

    feat: can make passwords visible (#81)
    * can make passwords visible
    
    * chore: trigger review
    
    * Update dashboard/src/components/PasswordInput.js
    
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
    
    Co-authored-by: Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
    Co-authored-by: Raphaël Huchet <raph@selego.co>
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>

---
+++ dashboard/src/components/PasswordInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import EyeIcon from '../assets/icons/EyeIcon';
+
+const PasswordInput = ({ InputComponent, showPassword, setShowPassword, type, ...props }) => (
+  <InputWithEye>
+    <InputComponent showPassword={showPassword} type={showPassword ? 'text' : 'password'} {...props} />
+    <EyeIcon strikedThrough={showPassword} onClick={() => setShowPassword(!showPassword)} />
+  </InputWithEye>
+);
+
+const InputWithEye = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+  margin-bottom: 0.375rem;
+  cursor: pointer;
+
+  > input {
+    margin-bottom: 0;
+  }
+
+  > svg {
+    position: absolute;
+    right: 15px;
+    margin-bottom: auto;
+  }
+`;
+
+export default PasswordInput;
