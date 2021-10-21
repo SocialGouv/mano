89c237e4fe3467866d42ba9ea0c0dd10cf837021
Raphaël Huchet <raph@selego.co>
2021-10-21 11:23:36 +0200

    feat(website): add matomo website

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,8 +1,19 @@
 import "tailwindcss/tailwind.css";
 import "style/default.css";
+import React from "react";
+import App from "next/app";
+import { init } from "@socialgouv/matomo-next";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const MATOMO_URL = "https://matomo.fabrique.social.gouv.fr/";
+const MATOMO_SITE_ID = "44";
+class MyApp extends App {
+  componentDidMount() {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
 
 export default MyApp;
