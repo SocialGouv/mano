204703cacb462e14d5634108ea948f4739325c9f
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2021-11-02 17:00:12 +0100

    feat(dashboard): show more stuff in organisations for superadmin to better pilot (#127)
    Co-authored-by: Arnaud AMBROSELLI <arnaudambroselli@pc72.home>

--- dashboard/src/scenes/organisation/list.js
+++ dashboard/src/scenes/organisation/list.js
@@ -1,7 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { Col, Container, FormGroup, Input, Modal, ModalBody, ModalHeader, Row, Label } from 'reactstrap';
 import { Formik } from 'formik';
 import { toastr } from 'react-redux-toastr';
+import styled from 'styled-components';
 import Table from '../../components/table';
 import ButtonCustom from '../../components/ButtonCustom';
 
@@ -9,29 +11,94 @@ import API from '../../services/api';
 
 import Loading from '../../components/loading';
 import CreateWrapper from '../../components/createWrapper';
-import { generatePassword } from '../../utils';
+import { generatePassword, toFrenchDate } from '../../utils';
 
 const List = () => {
   const [organisations, setOrganisation] = useState(null);
+  const [updateKey, setUpdateKey] = useState(null);
+  const [sortBy, setSortBy] = useState('createdAt');
+  const [sortOrder, setSortOrder] = useState('ASC');
   const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
     (async () => {
-      const { data } = await API.get({ path: '/organisation' });
-      setOrganisation(data);
+      const { data } = await API.get({ path: '/organisation', query: { withCounters: true } });
+      const sortedDataAscendant = data?.sort((org1, org2) => (org1[sortBy] > org2[sortBy] ? 1 : -1));
+      setOrganisation(sortOrder === 'ASC' ? sortedDataAscendant : [...sortedDataAscendant].reverse());
+      setUpdateKey((k) => k + 1);
     })();
   }, [refresh]);
 
+  useEffect(() => {
+    const sortedDataAscendant = organisations?.sort((org1, org2) => (org1[sortBy] > org2[sortBy] ? 1 : -1));
+    setOrganisation(sortOrder === 'ASC' ? sortedDataAscendant : [...sortedDataAscendant].reverse());
+    console.log(sortBy, sortOrder);
+    setUpdateKey((k) => k + 1);
+  }, [sortBy, sortOrder]);
+
   if (!organisations?.length) return <Loading />;
 
+  console.log(updateKey);
   return (
     <Container style={{ padding: '40px 0' }}>
       <Create onChange={() => setRefresh(true)} />
       <Table
         data={organisations}
+        // FIXME: Table is not updating without that key
+        // when we click on sortable arrows, I couldn't find why yet...
+        key={updateKey}
         columns={[
-          { title: 'Nom', dataKey: 'name' },
-          { title: 'Créée le', dataKey: 'createdAt' },
+          { title: 'Nom', dataKey: 'name', onSortOrder: setSortOrder, onSortBy: setSortBy, sortOrder, sortBy },
+          {
+            title: 'Créée le',
+            dataKey: 'createdAt',
+            render: (o) => toFrenchDate(o.createdAt || ''),
+            onSortOrder: setSortOrder,
+            onSortBy: setSortBy,
+            sortOrder,
+            sortBy,
+          },
+          {
+            title: 'Compteurs',
+            dataKey: 'counters',
+            sortableKey: 'countersTotal',
+            onSortOrder: setSortOrder,
+            onSortBy: setSortBy,
+            sortOrder,
+            sortBy,
+            render: (o) => {
+              return (
+                <StyledCounters total={o.countersTotal}>
+                  <span>Personnes: {o.counters.persons || 0}</span>
+                  <br />
+                  <span>Actions: {o.counters.actions || 0}</span>
+                  <br />
+                  <span>Territoires: {o.counters.territories || 0}</span>
+                  <br />
+                  <span>Comptes-rendus: {o.counters.reports || 0}</span>
+                  <br />
+                </StyledCounters>
+              );
+            },
+          },
+          {
+            title: 'Encryption activée',
+            dataKey: 'encryptionEnabled',
+            onSortOrder: setSortOrder,
+            onSortBy: setSortBy,
+            sortOrder,
+            sortBy,
+            render: (o) => (o.encryptionEnabled ? <input type="checkbox" checked disabled /> : null),
+          },
+          {
+            title: 'Dernière encryption',
+            dataKey: 'encryptionLastUpdateAt',
+            sortBy,
+            sortOrder,
+            onSortOrder: setSortOrder,
+            onSortBy: setSortBy,
+            render: (o) => toFrenchDate(o.encryptionLastUpdateAt || ''),
+          },
         ]}
         rowKey={'_id'}
         onRowClick={null}
@@ -39,6 +106,13 @@ const List = () => {
     </Container>
   );
 };
+
+const StyledCounters = styled.p`
+  ${(p) => p.total < 10 && 'opacity: 0.5;'}
+  ${(p) => p.total === 0 && 'opacity: 0.1;'}
+  ${(p) => p.total > 200 && 'font-weight: 600;'}
+  ${(p) => p.total > 1000 && 'font-weight: 800;'}
+`;
 
 const Create = ({ onChange }) => {
   const [open, setOpen] = useState(false);
