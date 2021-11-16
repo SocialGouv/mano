6044f8869aa84e9037049bab359f442413eb913f
Raphaël Huchet <raph@selego.co>
2021-11-16 12:34:07 +0100

    fix: fix observation as JSON

--- api/src/db/migrations/2021-11-16_observations-as-json.js
+++ api/src/db/migrations/2021-11-16_observations-as-json.js
@@ -3,16 +3,17 @@ const sequelize = require("../sequelize");
 
 (async () => {
   try {
-    const rows = await sequelize.query(`select * from "mano"."Organisation" where "customFieldsObs" is not null`, { type: QueryTypes.SELECT });
+    const rows = await sequelize.query(`select "customFieldsObs", _id from "mano"."Organisation" where "customFieldsObs" is not null`, {
+      type: QueryTypes.SELECT,
+    });
 
     // For each rows, check if the customFieldsObs is a JSON string
     // If it is, convert it to a JSON object
     // If it is not, do nothing
     for (const row of rows) {
-      if (typeof JSON.parse(row.customFieldsObs) === "string") {
-        const customFieldsObs = JSON.parse(row.customFieldsObs);
+      if (typeof row.customFieldsObs === "string") {
         await sequelize.query(`update "mano"."Organisation" set "customFieldsObs" = ? where _id = ?`, {
-          replacements: [customFieldsObs, row._id],
+          replacements: [row.customFieldsObs, row._id],
         });
       }
     }
