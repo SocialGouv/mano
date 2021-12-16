6eb089e3cb54bb045faedea57fa259075996b2c9
Raphaël Huchet <raph@selego.co>
2021-12-16 14:44:22 +0100

    chore: useless console.log

--- dashboard/src/scenes/team/view.js
+++ dashboard/src/scenes/team/view.js
@@ -63,7 +63,6 @@ const View = () => {
                     return response.data;
                   })
                 );
-                console.log(teams, response.data);
                 if (currentTeam._id === id) setCurrentTeam(response.data);
               }
             } catch (errorUpdatingTeam) {
