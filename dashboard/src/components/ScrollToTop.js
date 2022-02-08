63453823b7f915398e44a118c4aa7731f21f11d4
Raphaël Huchet <raph@selego.co>
2022-02-08 14:17:19 +0100

    fix(dashboard): fix iedge error for scroll

--- dashboard/src/components/ScrollToTop.js
+++ dashboard/src/components/ScrollToTop.js
@@ -5,7 +5,12 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    document?.querySelector('.main')?.scrollTo(0, 0);
+    // https://stackoverflow.com/questions/51517324/scrollto-method-doesnt-work-in-edge
+    if (document?.querySelector('.main')?.scrollTo) {
+      document.querySelector('.main').scrollTo(0, 0);
+    } else if (document?.querySelector('.main').scrollTop) {
+      document.querySelector('.main').scrollTop = 0;
+    }
   }, [pathname]);
 
   return null;
