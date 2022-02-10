3d58eee5ec3a1e1cfcb75255beb08e2e5258f290
Raphaël Huchet <raph@selego.co>
2022-02-10 15:46:38 +0100

    fix(dashboard): actions on connection

--- dashboard/src/scenes/auth/signin.js
+++ dashboard/src/scenes/auth/signin.js
@@ -54,7 +54,7 @@ const SignIn = () => {
     if (!!organisation?.receptionEnabled) {
       history.push('/reception');
     } else {
-      history.push('/');
+      history.push('/action');
     }
   };
 
