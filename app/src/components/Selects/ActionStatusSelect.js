08ecd77e9e19dd16b6a926f38d26d48d48fb0412
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-02-17 12:51:36 +0100

    fix(app): button done/cancel for action

--- app/src/components/Selects/ActionStatusSelect.js
+++ app/src/components/Selects/ActionStatusSelect.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
 import { CANCEL, CHOOSE, DONE, mappedIdsToLabels, TODO } from '../../recoil/actions';
 import colors from '../../utils/colors';
+import Button from '../Button';
 import SelectLabelled from './SelectLabelled';
 
 // prettier-ignore
@@ -11,6 +13,44 @@ const statuses = [
 ];
 
 const ActionStatusSelect = ({ value = CHOOSE, onSelect, onSelectAndSave, editable }) => {
+  if (!editable) {
+    return (
+      <Container>
+        <SelectLabelled
+          label="Status"
+          values={statuses}
+          mappedIdsToLabels={mappedIdsToLabels}
+          value={value}
+          onSelect={onSelect}
+          onSelectAndSave={onSelectAndSave}
+          editable={editable}
+        />
+        <ButtonsContainer>
+          {value !== CANCEL && (
+            <>
+              <Button
+                buttonSize={20}
+                borderColor="#00F"
+                color="#fff"
+                backgroundColor="#00F"
+                onPress={() => onSelectAndSave(CANCEL)}
+                caption={CANCEL}
+              />
+              <Spacer />
+            </>
+          )}
+          <Button
+            borderColor={value === TODO ? colors.app.color : null}
+            color={value === TODO ? '#fff' : colors.app.color}
+            buttonSize={20}
+            backgroundColor={value === TODO ? colors.app.color : null}
+            onPress={() => onSelectAndSave(value === TODO ? DONE : null)}
+            caption={value === TODO ? DONE : TODO}
+          />
+        </ButtonsContainer>
+      </Container>
+    );
+  }
   return (
     <SelectLabelled
       label="Status"
@@ -20,12 +60,21 @@ const ActionStatusSelect = ({ value = CHOOSE, onSelect, onSelectAndSave, editabl
       onSelect={onSelect}
       onSelectAndSave={onSelectAndSave}
       editable={editable}
-      buttonCaption={value === TODO ? 'FAIT' : null}
-      buttonValue={value === TODO ? DONE : null}
-      buttonBg={value === TODO ? colors.app.color : null}
-      buttonColor="#fff"
     />
   );
 };
+
+const Container = styled.View`
+  flex-direction: row;
+`;
+
+const ButtonsContainer = styled.View`
+  margin-top: 20px;
+  flex-direction: row;
+`;
+
+const Spacer = styled.View`
+  width: 10px;
+`;
 
 export default ActionStatusSelect;
