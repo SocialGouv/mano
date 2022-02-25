8c0038c5d999af1340863779a816048d025e7a01
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2022-02-25 16:53:06 +0100

    fix(app): text color on dark mode (#395)

--- app/src/components/MyText.js
+++ app/src/components/MyText.js
@@ -4,11 +4,13 @@ const MyText = styled.Text.attrs({ selectable: true })`
   font-family: NexaRegular;
   ${(props) => props.bold && 'font-family: Nexa-Bold;'}
   ${(props) => props.heavy && 'font-family: NexaHeavy;'}
+  color: #000000;
   ${(props) => props.color && `color: ${props.color};`}
 `;
 
 const MyTextInput = styled.TextInput`
   font-family: NexaRegular;
+  color: #000000;
 `;
 
 export { MyText, MyTextInput };
