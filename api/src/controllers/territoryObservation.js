9f4a50fd8e941d302029a0f20d761c07e280debb
Raphaël Huchet <raph@selego.co>
2022-03-10 22:28:08 +0100

    chore(security): territory controller

--- api/src/controllers/territoryObservation.js
+++ api/src/controllers/territoryObservation.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const { z } = require("zod");
+const { looseUuidRegex, positiveIntegerRegex } = require("../utils");
 const { catchErrors } = require("../errors");
 const TerritoryObservation = require("../models/territoryObservation");
 const { Op } = require("sequelize");
@@ -12,22 +14,34 @@ router.post(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   validateOrganisationEncryption,
-  catchErrors(async (req, res, next) => {
-    const newObs = {};
-    newObs.organisation = req.user.organisation;
-
-    if (!req.body.hasOwnProperty("encrypted") || !req.body.hasOwnProperty("encryptedEntityKey")) {
-      return next("No encrypted field in territoryObs create");
+  catchErrors(async (req, res) => {
+    try {
+      z.string().parse(req.body.encrypted);
+      z.string().parse(req.body.encryptedEntityKey);
+      if (req.body.createdAt) z.preprocess((input) => new Date(input), z.date()).parse(req.body.createdAt);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
     }
-
-    if (req.body.hasOwnProperty("encrypted")) newObs.encrypted = req.body.encrypted || null;
-    if (req.body.hasOwnProperty("encryptedEntityKey")) newObs.encryptedEntityKey = req.body.encryptedEntityKey || null;
+    const newObs = {
+      organisation: req.user.organisation,
+      encrypted: req.body.encrypted,
+      encryptedEntityKey: req.body.encryptedEntityKey,
+    };
     // FIXME: This "createdAt" pattern should be avoided. createdAt should not be updated.
     if (req.body.hasOwnProperty("createdAt")) newObs.createdAt = req.body.createdAt || null;
 
     const data = await TerritoryObservation.create(newObs, { returning: true });
-
-    return res.status(200).send({ ok: true, data });
+    return res.status(200).send({
+      ok: true,
+      data: {
+        _id: data._id,
+        encrypted: data.encrypted,
+        encryptedEntityKey: data.encryptedEntityKey,
+        organisation: data.organisation,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+      },
+    });
   })
 );
 
@@ -36,36 +50,30 @@ router.get(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   catchErrors(async (req, res) => {
+    try {
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.limit);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.page);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.lastRefresh);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+    const { limit, page, lastRefresh } = req.query;
+
     const query = {
-      where: {
-        organisation: req.user.organisation,
-      },
+      where: { organisation: req.user.organisation },
       order: [["createdAt", "DESC"]],
     };
 
-    const attributes = [
-      // Generic fields
-      "_id",
-      "encrypted",
-      "encryptedEntityKey",
-      "organisation",
-      "createdAt",
-      "updatedAt",
-    ];
-
-    if (req.query.lastRefresh) {
-      query.where.updatedAt = { [Op.gte]: new Date(Number(req.query.lastRefresh)) };
-      const data = await TerritoryObservation.findAll({ ...query, attributes });
-      return res.status(200).send({ ok: true, data });
-    }
-
     const total = await TerritoryObservation.count(query);
-    const limit = parseInt(req.query.limit, 10);
-    if (!!req.query.limit) query.limit = limit;
-    if (req.query.page) query.offset = parseInt(req.query.page, 10) * limit;
+    if (limit) query.limit = Number(limit);
+    if (page) query.offset = Number(page) * limit;
+    if (lastRefresh) query.where.updatedAt = { [Op.gte]: new Date(Number(lastRefresh)) };
 
-    const data = await TerritoryObservation.findAll({ ...query, attributes });
-    return res.status(200).send({ ok: true, data, hasMore: data.length === limit, total });
+    const data = await TerritoryObservation.findAll({
+      ...query,
+      attributes: ["_id", "encrypted", "encryptedEntityKey", "organisation", "createdAt", "updatedAt"],
+    });
+    return res.status(200).send({ ok: true, data, hasMore: data.length === Number(limit), total });
   })
 );
 
@@ -75,34 +83,45 @@ router.put(
   validateUser(["admin", "normal"]),
   validateOrganisationEncryption,
   catchErrors(async (req, res, next) => {
-    const query = {
-      where: {
-        _id: req.params._id,
-        organisation: req.user.organisation,
-      },
+    try {
+      z.string().regex(looseUuidRegex).parse(req.params._id);
+      if (req.body.createdAt) z.preprocess((input) => new Date(input), z.date()).parse(req.body.createdAt);
+      z.string().parse(req.body.encrypted);
+      z.string().parse(req.body.encryptedEntityKey);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+
+    const query = { where: { _id: req.params._id, organisation: req.user.organisation } };
+    const territoryObservation = await TerritoryObservation.findOne(query);
+    if (!territoryObservation) return res.status(404).send({ ok: false, error: "Not Found" });
+
+    const { createdAt, encrypted, encryptedEntityKey } = req.body;
+    const updatedTerritoryObservation = {
+      encrypted: encrypted,
+      encryptedEntityKey: encryptedEntityKey,
     };
-    const updateObs = {};
 
-    const observation = await TerritoryObservation.findOne(query);
-    if (!observation) return res.status(404).send({ ok: false, error: "Not found" });
-
-    if (!req.body.hasOwnProperty("encrypted") || !req.body.hasOwnProperty("encryptedEntityKey")) {
-      return next("No encrypted field in territoryObs create");
+    // FIXME: This pattern should be avoided. createdAt should be updated only when it is created.
+    if (createdAt) {
+      territoryObservation.changed("createdAt", true);
+      updatedTerritoryObservation.createdAt = new Date(createdAt);
     }
 
-    if (req.body.hasOwnProperty("encrypted")) updateObs.encrypted = req.body.encrypted || null;
-    if (req.body.hasOwnProperty("encryptedEntityKey")) updateObs.encryptedEntityKey = req.body.encryptedEntityKey || null;
+    await TerritoryObservation.update(updatedTerritoryObservation, query, { silent: false });
+    const newTerritoryObservation = await TerritoryObservation.findOne(query);
 
-    // FIXME: This "createdAt" pattern should be avoided. createdAt should not be updated.
-    if (req.body.hasOwnProperty("createdAt")) {
-      observation.changed("createdAt", true);
-      updateObs.createdAt = req.body.createdAt || null;
-    }
-
-    await TerritoryObservation.update(updateObs, query, { silent: false });
-    const newObservation = await TerritoryObservation.findOne(query);
-
-    return res.status(200).send({ ok: true, data: newObservation });
+    res.status(200).send({
+      ok: true,
+      data: {
+        _id: newTerritoryObservation._id,
+        encrypted: newTerritoryObservation.encrypted,
+        encryptedEntityKey: newTerritoryObservation.encryptedEntityKey,
+        organisation: newTerritoryObservation.organisation,
+        createdAt: newTerritoryObservation.createdAt,
+        updatedAt: newTerritoryObservation.updatedAt,
+      },
+    });
   })
 );
 
@@ -111,12 +130,12 @@ router.delete(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   catchErrors(async (req, res) => {
-    const query = {
-      where: {
-        _id: req.params._id,
-        organisation: req.user.organisation,
-      },
-    };
+    try {
+      z.string().regex(looseUuidRegex).parse(req.params._id);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+    const query = { where: { _id: req.params._id, organisation: req.user.organisation } };
 
     let observation = await TerritoryObservation.findOne(query);
     if (!observation) return res.status(404).send({ ok: false, error: "Not Found" });
