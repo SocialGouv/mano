fe52c28f707e5f7c14f3283c583af30b7b1be939
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2022-03-11 12:00:11 +0100

    chore(security): public controller (#466)

--- api/src/controllers/public.js
+++ api/src/controllers/public.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+const { z } = require("zod");
 const { catchErrors } = require("../errors");
-
 const Action = require("../models/action");
 const Comment = require("../models/comment");
 const Person = require("../models/person");
@@ -12,16 +11,25 @@ const TerritoryObservation = require("../models/territoryObservation");
 const Report = require("../models/report");
 const RelPersonPlace = require("../models/relPersonPlace");
 const { Op } = require("sequelize");
+const { positiveIntegerRegex, looseUuidRegex } = require("../utils");
 
 router.get(
   "/stats",
   catchErrors(async (req, res) => {
+    try {
+      if (req.query.organisation) z.string().regex(looseUuidRegex).parse(req.query.organisation);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.lastRefresh);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+
     const query = {};
     if (req.query.organisation) query.where = { organisation: req.query.organisation };
     if (Number(req.query.lastRefresh)) {
       if (!query.where) query.where = {};
       query.where.updatedAt = { [Op.gte]: new Date(Number(req.query.lastRefresh)) };
     }
+
     const places = await Place.count(query);
     const relsPersonPlace = await RelPersonPlace.count(query);
     const actions = await Action.count(query);
