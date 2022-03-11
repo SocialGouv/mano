e9d26c1a303e114f5f49eb454f92237cf860b327
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2022-03-11 14:59:51 +0100

    chore(security): report controller (#465)

--- api/src/controllers/report.js
+++ api/src/controllers/report.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const { Op } = require("sequelize");
+const { z } = require("zod");
+const { looseUuidRegex, positiveIntegerRegex } = require("../utils");
 const { catchErrors } = require("../errors");
 const validateOrganisationEncryption = require("../middleware/validateOrganisationEncryption");
 const validateUser = require("../middleware/validateUser");
@@ -12,36 +14,27 @@ router.get(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   catchErrors(async (req, res) => {
-    const query = {
-      where: {
-        organisation: req.user.organisation,
-      },
-    };
-
-    const attributes = [
-      // Generic fields
-      "_id",
-      "encrypted",
-      "encryptedEntityKey",
-      "organisation",
-      "createdAt",
-      "updatedAt",
-    ];
-
-    if (req.query.lastRefresh) {
-      query.where.updatedAt = { [Op.gte]: new Date(Number(req.query.lastRefresh)) };
-      const data = await Report.findAll({ ...query, attributes });
-      return res.status(200).send({ ok: true, data });
+    try {
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.limit);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.page);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.lastRefresh);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
     }
+    const { limit, page, lastRefresh } = req.query;
+
+    const query = { where: { organisation: req.user.organisation } };
 
     const total = await Report.count(query);
-    const limit = parseInt(req.query.limit, 10);
-    if (!!req.query.limit) query.limit = limit;
-    if (req.query.page) query.offset = parseInt(req.query.page, 10) * limit;
+    if (limit) query.limit = Number(limit);
+    if (page) query.offset = Number(page) * limit;
+    if (lastRefresh) query.where.updatedAt = { [Op.gte]: new Date(Number(lastRefresh)) };
 
-    const data = await Report.findAll({ ...query, attributes });
-
-    return res.status(200).send({ ok: true, data, hasMore: data.length === limit, total });
+    const data = await Report.findAll({
+      ...query,
+      attributes: ["_id", "encrypted", "encryptedEntityKey", "organisation", "createdAt", "updatedAt"],
+    });
+    return res.status(200).send({ ok: true, data, hasMore: data.length === Number(limit), total });
   })
 );
 
@@ -50,19 +43,33 @@ router.post(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   validateOrganisationEncryption,
-  catchErrors(async (req, res, next) => {
-    const newReport = { organisation: req.user.organisation };
-
-    if (!req.body.hasOwnProperty("encrypted") || !req.body.hasOwnProperty("encryptedEntityKey")) {
-      return next("No encrypted field in report create");
+  catchErrors(async (req, res) => {
+    try {
+      z.string().parse(req.body.encrypted);
+      z.string().parse(req.body.encryptedEntityKey);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
     }
 
-    if (req.body.hasOwnProperty("encrypted")) newReport.encrypted = req.body.encrypted || null;
-    if (req.body.hasOwnProperty("encryptedEntityKey")) newReport.encryptedEntityKey = req.body.encryptedEntityKey || null;
-
-    const reportData = await Report.create(newReport, { returning: true });
-
-    return res.status(200).send({ ok: true, data: reportData });
+    const data = await Report.create(
+      {
+        organisation: req.user.organisation,
+        encrypted: req.body.encrypted,
+        encryptedEntityKey: req.body.encryptedEntityKey,
+      },
+      { returning: true }
+    );
+    return res.status(200).send({
+      ok: true,
+      data: {
+        _id: data._id,
+        encrypted: data.encrypted,
+        encryptedEntityKey: data.encryptedEntityKey,
+        organisation: data.organisation,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+      },
+    });
   })
 );
 
@@ -71,23 +78,35 @@ router.put(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   validateOrganisationEncryption,
-  catchErrors(async (req, res, next) => {
-    const where = { _id: req.params._id };
-
-    const report = await Report.findOne({ where });
+  catchErrors(async (req, res) => {
+    try {
+      z.string().regex(looseUuidRegex).parse(req.params._id);
+      z.string().parse(req.body.encrypted);
+      z.string().parse(req.body.encryptedEntityKey);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+    const query = { where: { _id: req.params._id, organisation: req.user.organisation } };
+    const report = await Report.findOne(query);
     if (!report) return res.status(404).send({ ok: false, error: "Not Found" });
 
-    const updatedReport = {};
-    if (!req.body.hasOwnProperty("encrypted") || !req.body.hasOwnProperty("encryptedEntityKey")) {
-      return next("No encrypted field in report update");
-    }
-
-    if (req.body.hasOwnProperty("encrypted")) updatedReport.encrypted = req.body.encrypted || null;
-    if (req.body.hasOwnProperty("encryptedEntityKey")) updatedReport.encryptedEntityKey = req.body.encryptedEntityKey || null;
-
-    report.set(updatedReport);
+    const { encrypted, encryptedEntityKey } = req.body;
+    report.set({
+      encrypted: encrypted,
+      encryptedEntityKey: encryptedEntityKey,
+    });
     await report.save();
-    return res.status(200).send({ ok: true, data: report });
+    return res.status(200).send({
+      ok: true,
+      data: {
+        _id: report._id,
+        encrypted: report.encrypted,
+        encryptedEntityKey: report.encryptedEntityKey,
+        organisation: report.organisation,
+        createdAt: report.createdAt,
+        updatedAt: report.updatedAt,
+      },
+    });
   })
 );
 
@@ -96,12 +115,12 @@ router.delete(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   catchErrors(async (req, res) => {
-    const query = {
-      where: {
-        _id: req.params._id,
-        organisation: req.user.organisation,
-      },
-    };
+    try {
+      z.string().regex(looseUuidRegex).parse(req.params._id);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+    const query = { where: { _id: req.params._id, organisation: req.user.organisation } };
 
     const report = await Report.findOne(query);
     if (!report) return res.status(200).send({ ok: true });
