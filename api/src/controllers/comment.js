2bfb115a0d67bd489fdba07ea4177533d1ce02ee
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2022-03-11 14:59:03 +0100

    chore(security): comment controller (#459)

--- api/src/controllers/comment.js
+++ api/src/controllers/comment.js
@@ -2,30 +2,46 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const { Op } = require("sequelize");
+const { z } = require("zod");
 const { catchErrors } = require("../errors");
 const Comment = require("../models/comment");
 const validateOrganisationEncryption = require("../middleware/validateOrganisationEncryption");
 const validateUser = require("../middleware/validateUser");
+const { looseUuidRegex, positiveIntegerRegex } = require("../utils");
 
 router.post(
   "/",
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   validateOrganisationEncryption,
-  catchErrors(async (req, res, next) => {
-    const newComment = {};
-
-    newComment.organisation = req.user.organisation;
-
-    if (!req.body.hasOwnProperty("encrypted") || !req.body.hasOwnProperty("encryptedEntityKey")) {
-      return next("No encrypted field in comment creation");
+  catchErrors(async (req, res) => {
+    try {
+      z.string().parse(req.body.encrypted);
+      z.string().parse(req.body.encryptedEntityKey);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
     }
-    if (req.body.hasOwnProperty("encrypted")) newComment.encrypted = req.body.encrypted || null;
-    if (req.body.hasOwnProperty("encryptedEntityKey")) newComment.encryptedEntityKey = req.body.encryptedEntityKey || null;
 
-    const data = await Comment.create(newComment, { returning: true });
+    const data = await Comment.create(
+      {
+        organisation: req.user.organisation,
+        encrypted: req.body.encrypted,
+        encryptedEntityKey: req.body.encryptedEntityKey,
+      },
+      { returning: true }
+    );
 
-    return res.status(200).send({ ok: true, data });
+    return res.status(200).send({
+      ok: true,
+      data: {
+        _id: data._id,
+        encrypted: data.encrypted,
+        encryptedEntityKey: data.encryptedEntityKey,
+        organisation: data.organisation,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+      },
+    });
   })
 );
 
@@ -34,34 +50,30 @@ router.get(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   catchErrors(async (req, res) => {
+    try {
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.limit);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.page);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.lastRefresh);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+    const { limit, page, lastRefresh } = req.query;
+
     const query = {
-      where: {
-        organisation: req.user.organisation,
-      },
+      where: { organisation: req.user.organisation },
       order: [["createdAt", "DESC"]],
     };
-    if (req.query.lastRefresh) {
-      query.where.updatedAt = { [Op.gte]: new Date(Number(req.query.lastRefresh)) };
-    }
 
     const total = await Comment.count(query);
-    const limit = parseInt(req.query.limit, 10);
-    if (!!req.query.limit) query.limit = limit;
-    if (req.query.page) query.offset = parseInt(req.query.page, 10) * limit;
+    if (limit) query.limit = Number(limit);
+    if (page) query.offset = Number(page) * limit;
+    if (lastRefresh) query.where.updatedAt = { [Op.gte]: new Date(Number(lastRefresh)) };
 
     const data = await Comment.findAll({
       ...query,
-      attributes: [
-        // Generic fields
-        "_id",
-        "encrypted",
-        "encryptedEntityKey",
-        "organisation",
-        "createdAt",
-        "updatedAt",
-      ],
+      attributes: ["_id", "encrypted", "encryptedEntityKey", "organisation", "createdAt", "updatedAt"],
     });
-    return res.status(200).send({ ok: true, data, hasMore: data.length === limit, total });
+    return res.status(200).send({ ok: true, data, hasMore: data.length === Number(limit), total });
   })
 );
 
@@ -70,30 +82,46 @@ router.put(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   validateOrganisationEncryption,
-  catchErrors(async (req, res, next) => {
-    if (!req.body.comment) return res.status(400).send({ ok: false, error: "Comment is missing" });
-
+  catchErrors(async (req, res) => {
+    try {
+      z.string().regex(looseUuidRegex).parse(req.params._id);
+      if (req.body.createdAt) z.preprocess((input) => new Date(input), z.date()).parse(req.body.createdAt);
+      z.string().parse(req.body.encrypted);
+      z.string().parse(req.body.encryptedEntityKey);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
     const query = { where: { _id: req.params._id, organisation: req.user.organisation } };
     const comment = await Comment.findOne(query);
     if (!comment) return res.status(404).send({ ok: false, error: "Not Found" });
 
-    const updateComment = {};
+    const { createdAt, encrypted, encryptedEntityKey } = req.body;
 
-    if (!req.body.hasOwnProperty("encrypted") || !req.body.hasOwnProperty("encryptedEntityKey")) {
-      return next("No encrypted field in comment update");
-    }
-    if (req.body.hasOwnProperty("encrypted")) updateComment.encrypted = req.body.encrypted || null;
-    if (req.body.hasOwnProperty("encryptedEntityKey")) updateComment.encryptedEntityKey = req.body.encryptedEntityKey || null;
+    const updateComment = {
+      encrypted: encrypted,
+      encryptedEntityKey: encryptedEntityKey,
+    };
+
     // FIXME: This pattern should be avoided. createdAt should be updated only when it is created.
-    if (req.body.hasOwnProperty("createdAt") && !!req.body.createdAt) {
+    if (createdAt) {
       comment.changed("createdAt", true);
-      updateComment.createdAt = new Date(req.body.createdAt);
+      updateComment.createdAt = new Date(createdAt);
     }
 
     await Comment.update(updateComment, query, { silent: false });
     const newComment = await Comment.findOne(query);
 
-    return res.status(200).send({ ok: true, data: newComment });
+    return res.status(200).send({
+      ok: true,
+      data: {
+        _id: newComment._id,
+        encrypted: newComment.encrypted,
+        encryptedEntityKey: newComment.encryptedEntityKey,
+        organisation: newComment.organisation,
+        createdAt: newComment.createdAt,
+        updatedAt: newComment.updatedAt,
+      },
+    });
   })
 );
 
@@ -102,12 +130,12 @@ router.delete(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   catchErrors(async (req, res) => {
-    const query = {
-      where: {
-        _id: req.params._id,
-        organisation: req.user.organisation,
-      },
-    };
+    try {
+      z.string().regex(looseUuidRegex).parse(req.params._id);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+    const query = { where: { _id: req.params._id, organisation: req.user.organisation } };
 
     const comment = await Comment.findOne(query);
     if (!comment) return res.status(200).send({ ok: true });
