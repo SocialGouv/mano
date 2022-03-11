5bec69999f47807a0089c0c1a7e89eb84878adf6
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2022-03-11 14:59:31 +0100

    chore(security): person controller (#461)
    * chore(security): person controller
    
    * chore: up with relperson
    
    * fix: fix import

--- api/src/controllers/relPersonPlace.js
+++ api/src/controllers/relPersonPlace.js
@@ -2,34 +2,46 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const { Op } = require("sequelize");
+const { z } = require("zod");
 const { catchErrors } = require("../errors");
 const validateOrganisationEncryption = require("../middleware/validateOrganisationEncryption");
 const validateUser = require("../middleware/validateUser");
 const RelPersonPlace = require("../models/relPersonPlace");
+const { looseUuidRegex, positiveIntegerRegex } = require("../utils");
 
 router.post(
   "/",
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   validateOrganisationEncryption,
-  catchErrors(async (req, res, next) => {
-    const { person, place } = req.body;
-    if (!person || !place) return res.status(400).send({ ok: false, error: "Missing place or person" });
-
-    const newRelPersonPlace = {};
-
-    newRelPersonPlace.organisation = req.user.organisation;
-
-    if (!req.body.hasOwnProperty("encrypted") || !req.body.hasOwnProperty("encryptedEntityKey")) {
-      return next("No encrypted field in relPersonPlace create");
+  catchErrors(async (req, res) => {
+    try {
+      z.string().parse(req.body.encrypted);
+      z.string().parse(req.body.encryptedEntityKey);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
     }
 
-    if (req.body.hasOwnProperty("encrypted")) newRelPersonPlace.encrypted = req.body.encrypted || null;
-    if (req.body.hasOwnProperty("encryptedEntityKey")) newRelPersonPlace.encryptedEntityKey = req.body.encryptedEntityKey || null;
+    const data = await RelPersonPlace.create(
+      {
+        organisation: req.user.organisation,
+        encrypted: req.body.encrypted,
+        encryptedEntityKey: req.body.encryptedEntityKey,
+      },
+      { returning: true }
+    );
 
-    const data = await RelPersonPlace.create(newRelPersonPlace, { returning: true });
-
-    return res.status(200).send({ ok: true, data });
+    return res.status(200).send({
+      ok: true,
+      data: {
+        _id: data._id,
+        encrypted: data.encrypted,
+        encryptedEntityKey: data.encryptedEntityKey,
+        organisation: data.organisation,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+      },
+    });
   })
 );
 
@@ -38,34 +50,30 @@ router.get(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   catchErrors(async (req, res) => {
+    try {
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.limit);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.page);
+      z.optional(z.string().regex(positiveIntegerRegex)).parse(req.query.lastRefresh);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+    const { limit, page, lastRefresh } = req.query;
+
     const query = {
-      where: {
-        organisation: req.user.organisation,
-      },
+      where: { organisation: req.user.organisation },
       order: [["createdAt", "DESC"]],
     };
-    if (req.query.lastRefresh) {
-      query.where.updatedAt = { [Op.gte]: new Date(Number(req.query.lastRefresh)) };
-    }
 
     const total = await RelPersonPlace.count(query);
-    const limit = parseInt(req.query.limit, 10);
-    if (!!req.query.limit) query.limit = limit;
-    if (req.query.page) query.offset = parseInt(req.query.page, 10) * limit;
+    if (limit) query.limit = Number(limit);
+    if (page) query.offset = Number(page) * limit;
+    if (lastRefresh) query.where.updatedAt = { [Op.gte]: new Date(Number(lastRefresh)) };
 
     const data = await RelPersonPlace.findAll({
       ...query,
-      attributes: [
-        // Generic fields
-        "_id",
-        "encrypted",
-        "encryptedEntityKey",
-        "organisation",
-        "createdAt",
-        "updatedAt",
-      ],
+      attributes: ["_id", "encrypted", "encryptedEntityKey", "organisation", "createdAt", "updatedAt"],
     });
-    return res.status(200).send({ ok: true, data, hasMore: data.length === limit, total });
+    return res.status(200).send({ ok: true, data, hasMore: data.length === Number(limit), total });
   })
 );
 
@@ -74,8 +82,12 @@ router.delete(
   passport.authenticate("user", { session: false }),
   validateUser(["admin", "normal"]),
   catchErrors(async (req, res) => {
-    const { _id } = req.params;
-    await RelPersonPlace.destroy({ where: { _id } });
+    try {
+      z.string().regex(looseUuidRegex).parse(req.params._id);
+    } catch (e) {
+      return res.status(400).send({ ok: false, error: "Invalid request" });
+    }
+    await RelPersonPlace.destroy({ where: { _id: req.params._id, organisation: req.user.organisation } });
     res.status(200).send({ ok: true });
   })
 );
