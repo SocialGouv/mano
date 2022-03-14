196aa61b9af37611db7984a0c45a372ddfdf936c
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-03-14 15:46:47 +0100

    dashboard: handle new passage ongoing

---
+++ dashboard/src/recoil/passages.js
@@ -0,0 +1,26 @@
+import { atom } from 'recoil';
+
+export const passagesState = atom({
+  key: 'passagesState',
+  default: [],
+});
+
+const encryptedFields = ['person', 'team', 'user'];
+
+export const preparePassageForEncryption = (passage) => {
+  const decrypted = {};
+  for (let field of encryptedFields) {
+    decrypted[field] = passage[field];
+  }
+  return {
+    _id: passage._id,
+    createdAt: passage.createdAt,
+    updatedAt: passage.updatedAt,
+    organisation: passage.organisation,
+
+    decrypted,
+    entityKey: passage.entityKey,
+
+    ...passage,
+  };
+};
