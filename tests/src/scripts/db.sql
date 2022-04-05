bd410e5e743b9a2cf0e0d9146fa9378036a3fa0a
Raphaël Huchet <raph@selego.co>
2022-04-05 10:12:37 +0200

    chore: oops

--- tests/src/scripts/db.sql
+++ tests/src/scripts/db.sql
@@ -33,7 +33,7 @@ create table if not exists "mano"."Organisation"
     "createdAt"                timestamp with time zone not null,
     "updatedAt"                timestamp with time zone not null,
     categories                 text[],
-"consultations"            jsonb default '[{ "name": "Médicale", "fields": [{ "name": "description", "type": "textarea", "label": "Description", "enabled": true, "showInStats": false }] }]'::jsonb,
+    "consultations"            jsonb default '[{ "name": "Médicale", "fields": [{ "name": "description", "type": "textarea", "label": "Description", "enabled": true, "showInStats": false }] }]'::jsonb,
     "encryptionEnabled"        boolean default false,
     "encryptionLastUpdateAt"   timestamp with time zone,
     encrypting                 boolean default false,
