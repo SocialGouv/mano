2c2844cf67adc8a03d07ddcfabc5faba3fbbc476
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-04-07 16:35:10 +0200

    component ldap instead of odic

--- dashboard/src/scenes/ldap/index.js
+++ dashboard/src/scenes/ldap/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Oidc from "./oidc"
+import Ldap from "./ldap"
 const Router = () => {
   return (
     <Switch>
-      <Route path="/ldap" component={Oidc} />
+      <Route path="/ldap" component={Ldap} />
     </Switch>
   );
 };
