473121cc01c06e3fb30a34a3d99c378d1a258aa4
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-04-11 15:53:39 +0200

    chore: show total in pagination

--- dashboard/src/components/pagination.js
+++ dashboard/src/components/pagination.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Pagination from "react-js-pagination";
-import { Col, Row } from "reactstrap";
+import React from 'react';
+import Pagination from 'react-js-pagination';
+import { Col, Row } from 'reactstrap';
 
 const Page = ({ page = 0, limit = 20, total = 0, onChange = () => null }) => (
   <Row>
     <Col md={12} style={{ marginTop: 10, marginBottom: 10 }}>
-      <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '1rem' }}>
         <Pagination
           itemClass="page-item"
           linkClass="page-link"
@@ -15,6 +15,7 @@ const Page = ({ page = 0, limit = 20, total = 0, onChange = () => null }) => (
           pageRangeDisplayed={5}
           onChange={(page) => onChange({ limit, total, page: page - 1 })}
         />
+        <i style={{ marginLeft: '1rem', marginBottom: '1rem', opacity: 0.5 }}>(Total: {total})</i>
       </div>
     </Col>
   </Row>
