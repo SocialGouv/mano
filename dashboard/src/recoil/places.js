cabb88d0c3518f94f3b8a50c6b37bc5caa018ded
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2022-04-22 11:34:48 +0200

    Update dashboard/src/recoil/places.js
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>

--- dashboard/src/recoil/places.js
+++ dashboard/src/recoil/places.js
@@ -4,10 +4,6 @@ import { atom } from 'recoil';
 const collectionName = 'place';
 export const placesState = atom({
   key: collectionName,
-  /* default: new Promise(async (resolve) => {
-    const cache = await localforage.getItem(collectionName);
-    resolve(cache || []);
-  }), */
   default: [],
   effects: [
     ({ onSet }) => {
