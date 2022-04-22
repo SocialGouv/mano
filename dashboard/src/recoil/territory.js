c04efd3dafc41178e54a7d0b75002ad776d1551b
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2022-04-22 11:34:41 +0200

    Update dashboard/src/recoil/territory.js
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>

--- dashboard/src/recoil/territory.js
+++ dashboard/src/recoil/territory.js
@@ -4,10 +4,6 @@ import { atom } from 'recoil';
 const collectionName = 'territory';
 export const territoriesState = atom({
   key: collectionName,
-  /* default: new Promise(async (resolve) => {
-    const cache = await localforage.getItem(collectionName);
-    resolve(cache || []);
-  }), */
   default: [],
   effects: [
     ({ onSet }) => {
