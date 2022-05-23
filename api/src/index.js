fb5c23d3fbd634d277a1f868b96c112e63e7686c
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2022-05-23 10:15:58 +0200

    feat(api): pen test better data (#703)
    * feat(api): pen test better data
    
    * fix: mano.fr to gmx.fr

--- api/src/index.js
+++ api/src/index.js
@@ -26,6 +26,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(cors({ credentials: true, origin: ["http://localhost:4145", "http://localhost:8083", "http://localhost:8090"] }));
 }
 
+if (process.env.NODE_ENV === "production") {
+  console.log("coucou");
+}
+
 const now = new Date();
 // kube probe
 app.get("/healthz", async (req, res) => {
