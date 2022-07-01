d609d770d3601406f491de4609f8659f6da45661
Raphaël Huchet <raph@selego.co>
2022-07-01 14:17:27 +0200

    fix: up

--- app/src/components/Selects/ConsultationTypeSelect.js
+++ app/src/components/Selects/ConsultationTypeSelect.js
@@ -7,7 +7,7 @@ const ConsultationTypeSelect = ({ value, onSelect, editable }) => {
   const organisation = useRecoilValue(organisationState);
   const types = ['-- Choisissez un type  --', ...organisation.consultations.map((t) => t.name)];
   if (!value?.length) value = types[0];
-  return <SelectLabelled label="Motif de sortie de file active" values={types} value={value} onSelect={onSelect} editable={editable} />;
+  return <SelectLabelled label="Type" values={types} value={value} onSelect={onSelect} editable={editable} />;
 };
 
 export default ConsultationTypeSelect;
