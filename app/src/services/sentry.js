2c8950847d33b7846c60bd82cab2fff727ed4c66
Raphaël Huchet <raph@selego.co>
2022-07-15 15:02:22 +0200

    fix(security): remove password from extra info in sentry

--- app/src/services/sentry.js
+++ app/src/services/sentry.js
@@ -4,12 +4,17 @@ import * as Sentry from '@sentry/react-native';
 
 export const capture = (err, context = {}) => {
   console.log('capture', err, JSON.stringify(context, null, 2));
+
   if (typeof context === 'string') {
     context = JSON.parse(context);
   } else {
     context = JSON.parse(JSON.stringify(context));
   }
   if (context?.extra?.response?.status === 401) return;
+
+  if (context?.extra?.body?.password) {
+    context.extra.body.password = '******';
+  }
   if (Sentry && err) {
     if (typeof err === 'string') {
       Sentry.captureMessage(err, context);
