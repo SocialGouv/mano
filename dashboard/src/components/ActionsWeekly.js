5df7516bcbde358922acc434a97772b462d06282
Raphaël Huchet <raph@selego.co>
2022-07-15 13:21:39 +0200

    feat: weekly calendar

--- dashboard/src/components/ActionsWeekly.js
+++ dashboard/src/components/ActionsWeekly.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
-import { Button } from 'reactstrap';
+import React, { useMemo, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Badge, Button } from 'reactstrap';
 import styled from 'styled-components';
-import { dayjsInstance, isOnSameDay } from '../services/date';
+import { dayjsInstance, formatTime, isOnSameDay } from '../services/date';
+import ActionStatus from './ActionStatus';
+import ExclamationMarkButton from './ExclamationMarkButton';
+import PersonName from './PersonName';
+
+// TODO: remove inline style when UI is stabilized.
 
 export default function ActionsWeekly({ actions }) {
   const [startOfWeek, setStartOfWeek] = useState(dayjsInstance().startOf('week'));
+  const actionsInWeek = useMemo(() => {
+    return actions.filter((action) => dayjsInstance(action.dueAt).isBetween(startOfWeek, startOfWeek.add(7, 'day').endOf('day')));
+  }, [actions, startOfWeek]);
   return (
     <div>
       <div style={{ display: 'flex', flexDirection: 'row', gap: '2rem', marginBottom: '1rem', alignItems: 'center' }}>
@@ -27,7 +36,7 @@ export default function ActionsWeekly({ actions }) {
           const isToday = day.isSame(dayjsInstance(), 'day');
           return (
             <div key={day.format('YYYY-MM-DD')}>
-              <div style={{ textAlign: 'center', marginTop: '5px' }}>
+              <div style={{ textAlign: 'center', marginTop: '5px', marginBottom: '5px' }}>
                 <div style={{ fontSize: '11px', ...(isToday ? { color: '#1a73e8' } : {}) }}>{day.format('ddd')}</div>
                 <div
                   style={{
@@ -48,11 +57,83 @@ export default function ActionsWeekly({ actions }) {
                   {day.format('D')}
                 </div>
               </div>
-              <div>{(actions || []).filter((a) => isOnSameDay(a.dueAt, day)).length}</div>
+              <ActionsOfDay actions={actionsInWeek.filter((action) => isOnSameDay(action.dueAt, day))} />
             </div>
           );
         })}
       </WeekContainer>
+    </div>
+  );
+}
+
+function ActionsOfDay({ actions }) {
+  const history = useHistory();
+
+  const sortedActions = [
+    // Urgent actions first
+    ...actions.filter((action) => action.urgent),
+    // Then actions with time ordered by dueAt
+    ...actions
+      .filter((action) => !action.urgent)
+      .filter((action) => Boolean(action.withTime))
+      .sort((a, b) => a.dueAt - b.dueAt),
+    // Then actions without time.
+    ...actions.filter((action) => !action.urgent).filter((action) => !action.withTime),
+  ];
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', marginBottom: '1rem' }}>
+      {sortedActions.map((action) => (
+        <div
+          onClick={() => {
+            if (action.isConsultation) {
+              history.push(`/person/${action.person}?tab=dossier+médical&consultationId=${action._id}`);
+            } else {
+              history.push(`/action/${action._id}`);
+            }
+          }}
+          style={{
+            border: '1px solid #ccc',
+            backgroundColor: Boolean(action.isConsultation) ? '#DDF4FF' : Boolean(action.urgent) ? '#fecaca' : '#fafafa',
+            borderRadius: '3px',
+            padding: '3px',
+            fontSize: '12px',
+            gap: '0.5rem',
+            display: 'flex',
+            flexDirection: 'column',
+          }}>
+          {(Boolean(action.isConsultation) || Boolean(action.urgent)) && (
+            <div>
+              {Boolean(action.urgent) && (
+                <div style={{ display: 'flex', flexDirection: 'row', fontWeight: 'bold', color: '#dc2626', alignItems: 'center', gap: '10px' }}>
+                  <ExclamationMarkButton />
+                  Urgent
+                </div>
+              )}
+              {Boolean(action.isConsultation) && (
+                <div style={{ display: 'flex', flexDirection: 'row', fontWeight: 'bold', color: '#43738b' }}>
+                  <i>🩺 Consultation</i>
+                </div>
+              )}
+            </div>
+          )}
+          <div>
+            <div>{action.name}</div>
+            <div>
+              {action.categories?.map((category) => (
+                <Badge style={{ margin: '2px', fontSize: '11px', fontWeight: 'normal', whiteSpace: 'break-spaces' }} color="info" key={category}>
+                  {category}
+                </Badge>
+              ))}
+            </div>
+          </div>
+          {Boolean(action.withTime) && <div>🕑 {formatTime(action.dueAt)}</div>}
+          <div>
+            🧑 <PersonName item={action} />
+          </div>
+          <ActionStatus status={action.status} />
+        </div>
+      ))}
     </div>
   );
 }
