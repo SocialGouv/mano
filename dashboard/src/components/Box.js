88a8dc7277f9cec44a98c6ef6f7b7efadd125f7c
Raphaël Huchet <raph@selego.co>
2022-07-19 11:29:43 +0200

    chore: remove weird label style (which was used randomly)

--- dashboard/src/components/Box.js
+++ dashboard/src/components/Box.js
@@ -5,11 +5,5 @@ const Box = styled.div`
   /* box-shadow: 0px 4px 8px rgba(29, 32, 33, 0.02); */
   border-radius: 8px;
   width: 100%;
-  label {
-    margin-bottom: 3px;
-    font-weight: 800;
-    text-transform: uppercase;
-    font-size: 12px;
-  }
 `;
 export default Box;
