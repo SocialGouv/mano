1e455f1acf7dc3e7cf569cb6a12cfe16e136b25c
Raphaël Huchet <raph@selego.co>
2022-08-11 15:17:48 +0200

    chore: use ref

--- dashboard/src/components/Loader.js
+++ dashboard/src/components/Loader.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { getCacheItem, getData, setCacheItem } from '../services/dataManagement';
