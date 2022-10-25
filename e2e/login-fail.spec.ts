48d31be6184349f661f6cf6e167b7c2b170fc078
Raphaël Huchet <raph@selego.co>
2022-10-25 11:49:53 +0200

    chore: up

--- e2e/login-fail.spec.ts
+++ e2e/login-fail.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("Try to login and fail", async ({ page }) => {
   await page.goto("http://localhost:8090/");
   await page.goto("http://localhost:8090/auth");
 
