6b59409ae1c9e53183459fe8861b0af9ecf0bf18
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-11-07 05:25:08 +0100

    chore: add NativeWind to React Native app

--- app/src/components/RowContainer.js
+++ app/src/components/RowContainer.js
@@ -1,44 +1,18 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 
 const RowContainer = ({ Component = TouchableOpacity, onPress, disabled, noPadding, children, center, testID = '', styles: stylesProps = {} }) => {
   return (
     <Component onPress={onPress} disabled={disabled} testID={testID}>
-      <View style={[styles.container, stylesProps?.container]}>
-        <View style={[styles.subContainer, noPadding && styles.noPadding, center && styles.center, stylesProps?.subContainer]}>{children}</View>
+      <View className="overflow-hidden rounded-2xl bg-[#f4f5f8] mx-2.5 mb-2.5" style={stylesProps?.container}>
+        <View
+          className={['py-5 px-3 items-center flex-row w-full', noPadding && 'p-0', center && 'justify-center'].join(' ')}
+          style={stylesProps?.subContainer}>
+          {children}
+        </View>
       </View>
     </Component>
   );
 };
-
-// seems to be a problem with the nested shadow-offset and styled-components
-// switching temporarily to stylesheet for this
-const styles = StyleSheet.create({
-  container: {
-    overflow: 'hidden',
-    borderRadius: 16,
-    backgroundColor: '#f4f5f8',
-    marginHorizontal: 10,
-    marginBottom: 10,
-    // shadowColor: '#000',
-    // shadowOffset: { width: 0, height: 3 },
-    // shadowRadius: 5,
-    // shadowOpacity: 0.55,
-    // elevation: 10,
-  },
-  subContainer: {
-    paddingVertical: 20,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    flexDirection: 'row',
-    width: '100%',
-  },
-  noPadding: {
-    padding: 0,
-  },
-  center: {
-    justifyContent: 'center',
-  },
-});
 
 export default RowContainer;
