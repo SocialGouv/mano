43abb149a6cbf12362d0c8f3b51e12491f013665
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-11-22 14:30:36 +0100

    update TeamTag to remove ...props + add tailwind

--- dashboard/src/components/TagTeam.js
+++ dashboard/src/components/TagTeam.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
-import styled from 'styled-components';
 import { teamsState } from '../recoil/auth';
 
-const TagTeam = ({ teamId, ...props }) => {
+const TagTeam = ({ teamId, className = '' }) => {
   const teams = useRecoilValue(teamsState);
   const teamIndex = teams?.findIndex((t) => t._id === teamId);
   const team = teams?.find((t) => t._id === teamId);
   if (!team) return null;
   return (
-    <Team key={team?._id} teamIndex={teamIndex} {...props}>
+    <div
+      key={team?._id}
+      style={{
+        backgroundColor: teamsColors[teamIndex % teamsColors?.length],
+        borderColor: borderColors[teamIndex % borderColors?.length],
+      }}
+      className={['tw-rounded tw-border tw-py-0.5 tw-px-2.5 tw-text-center tw-text-xs tw-text-white', className].join(' ')}>
       {team?.name}
-    </Team>
+    </div>
   );
 };
 
 const teamsColors = ['#255c99cc', '#74776bcc', '#00c6a5cc', '#ff4b64cc', '#ef798acc'];
 const borderColors = ['#255c99', '#74776b', '#00c6a5', '#ff4b64', '#ef798a'];
-
-const Team = styled.div`
-  font-size: 0.75rem;
-  text-align: center;
-  background-color: ${({ teamIndex }) => teamsColors[teamIndex % teamsColors?.length]};
-  border: 1px ${({ teamIndex }) => borderColors[teamIndex % borderColors?.length]} solid;
-  padding: 2px 10px;
-  border-radius: 5px;
-  color: #fff;
-`;
 
 export default TagTeam;
