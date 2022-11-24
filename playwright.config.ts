d03ee0bb9bd8e12bcfd8b3243eefe0f69d1985dc
Raphaël Huchet <raph@selego.co>
2022-11-24 16:20:29 +0100

    chore: remove safari from tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,12 +63,12 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    {
-      name: "webkit",
-      use: {
-        ...devices["Desktop Safari"],
-      },
-    },
+    // {
+    //   name: "webkit",
+    //   use: {
+    //     ...devices["Desktop Safari"],
+    //   },
+    // },
 
     /* Test against mobile viewports. */
     // {
