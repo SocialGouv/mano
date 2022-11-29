7d5438ba7fd45930aa6bd5fbdbb058de752f6059
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-11-29 06:25:59 +0100

    fix: test

--- dashboard/src/components/ActionName.js
+++ dashboard/src/components/ActionName.js
@@ -4,19 +4,18 @@ import { userState } from '../recoil/auth';
 import { useRecoilValue } from 'recoil';
 import { getConsultationField } from '../recoil/consultations';
 
-export default function ActionName({ action }) {
+export default function ActionName({ action: actionOrConsultation }) {
   const me = useRecoilValue(userState);
-  console.log({ action });
   return (
     <>
-      <div>{!action?.isConsultation ? action?.name : getConsultationField(action, 'name', me)}</div>
+      <div>{!actionOrConsultation.isConsultation ? actionOrConsultation.name : getConsultationField(actionOrConsultation, 'name', me)}</div>
       <div>
-        {action?.categories?.map((category) => (
-          <Badge style={{ margin: '0 2px' }} color="info" key={category} data-test-id={action.name + category}>
+        {actionOrConsultation.categories?.map((category) => (
+          <Badge style={{ margin: '0 2px' }} color="info" key={category} data-test-id={actionOrConsultation.name + category}>
             {category}
           </Badge>
         ))}
-        {!!action?.isConsultation && <small className="text-muted">{getConsultationField(action, 'type', me)}</small>}
+        {!!actionOrConsultation.isConsultation && <small className="text-muted">{getConsultationField(actionOrConsultation, 'type', me)}</small>}
       </div>
     </>
   );
