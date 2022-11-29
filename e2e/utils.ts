d42261056354aeef405957769c99c2fbad365566
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-11-29 14:12:14 +0100

    fix: tests

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,12 +1,12 @@
 import { expect, Page } from "@playwright/test";
 
 export async function clickOnEmptyReactSelect(page: Page, name: string, text: string) {
-  await page.locator(`.${name}__placeholder`).click();
+  await page.locator(`.${name}__dropdown-indicator`).click();
   await page.locator(`.${name}__menu`).getByText(text, { exact: true }).click();
 }
 
 export async function changeReactSelectValue(page: Page, name: string, text: string) {
-  await page.locator(`.${name}__indicator`).nth(0).click();
+  await page.locator(`.${name}__dropdown-indicator`).click();
   await page.locator(`.${name}__menu`).getByText(text, { exact: true }).click();
 }
 
