3544ccff39a9e65ea28914861d36b3332285703f
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2022-12-01 15:57:15 +0100

    chore: testchecker (#1025)

---
+++ dashboard/src/components/TextChecker.js
@@ -0,0 +1,29 @@
+import { useRecoilValue } from 'recoil';
+import { reportsState } from '../recoil/reports';
+
+const TextChecker = () => {
+  const reports = useRecoilValue(reportsState);
+  const duplicateReports = Object.entries(
+    reports.reduce((reportsByDate, report) => {
+      if (!reportsByDate[`${report.date}-${report.team}`]) reportsByDate[`${report.date}-${report.team}`] = [];
+      reportsByDate[`${report.date}-${report.team}`].push(report);
+      return reportsByDate;
+    }, {})
+  ).filter(([key, reportsByDate]) => reportsByDate.length > 1);
+  if (duplicateReports.length === 0) return null;
+  return (
+    <div className="tw-fixed tw-inset-0 tw-z-[999999999] tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-3 tw-bg-red-500">
+      <p className="tw-text-4xl tw-font-bold tw-text-white">Many reports for the same team and date exists - you failed the test</p>
+      <img
+        src="https://media2.giphy.com/media/CkuExgcOmraBlFowpQ/giphy.gif?cid=6104955exbo839oh8np0ruy6x8ezdu1vrm45thbudvn2gq56&rid=giphy.gif&ct=g"
+        alt="you failed"
+      />
+      <details>
+        <summary>Show duplicates reports</summary>
+        <pre>{JSON.stringify(duplicateReports, null, 2)}</pre>
+      </details>
+    </div>
+  );
+};
+
+export default TextChecker;
