27c2fe20539460efa1b8c8701eb01ad7b1743955
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-12-12 15:20:57 +0100

    chore: edit custom field test

--- e2e/custom-field-edit.spec.ts
+++ e2e/custom-field-edit.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { nanoid } from "nanoid";
 import { populate } from "./scripts/populate-db";
-import { changeReactSelectValue, loginWith } from "./utils";
+import { changeReactSelectValue, clickOnEmptyReactSelect, loginWith } from "./utils";
 
 test.beforeAll(async () => {
   await populate();
@@ -20,6 +20,15 @@ test("test", async ({ page }) => {
   await page.getByText("Mise à jour !").click();
   await page.getByRole("button", { name: "Ajouter un champ" }).nth(1).click();
   await page.getByLabel("Nom").fill("Champ utilisé");
+  await page.locator(".type__input-container").click();
+  await page.locator("#react-select-type-option-7").click();
+  await page.locator(".options__input-container").click();
+  await page.getByLabel("Choix").fill("choix 1");
+  await page.getByLabel("Choix").press("Enter");
+  await page.getByLabel("Choix").fill("choix 2");
+  await page.getByLabel("Choix").press("Enter");
+  await page.getByLabel("Choix").fill("choix 3");
+  await page.getByLabel("Choix").press("Enter");
   await page.getByRole("button", { name: "Enregistrer" }).click();
   await page.getByText("Mise à jour !").click();
 
@@ -32,7 +41,7 @@ test("test", async ({ page }) => {
   await expect(page).toHaveURL(/http:\/\/localhost:8090\/person\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/);
 
   await page.getByRole("button", { name: "Éditer les informations médicales" }).click();
-  await page.getByLabel("Champ utilisé").fill("plouf");
+  await clickOnEmptyReactSelect(page, "person-custom-select-champ-utilisé", "choix 1");
   await page.getByRole("button", { name: "Enregistrer" }).click();
   await page.getByText("Mis à jour !").click();
 
@@ -43,7 +52,10 @@ test("test", async ({ page }) => {
 
   await page.locator('[data-test-id="Champ utilisé"]').getByRole("button", { name: "Modifier le champ" }).click();
   await expect(page.locator(".type--is-disabled")).toBeVisible();
-  await page.getByRole("button", { name: "Annuler" }).click();
+  await page.getByLabel("Nom").fill("Champ utilisé et modifié");
+  await page.getByRole("button", { name: "Enregistrer" }).click();
+  await expect(page.getByText("choix 1, choix 2, choix 3")).toBeVisible();
+  await expect(page.getByRole("cell", { name: "Champ utilisé et modifié" })).toBeVisible();
 
   await page.locator('[data-test-id="Champ non utilisé"]').getByRole("button", { name: "Modifier le champ" }).click();
   await expect(page.locator(".type--is-disabled")).not.toBeVisible();
