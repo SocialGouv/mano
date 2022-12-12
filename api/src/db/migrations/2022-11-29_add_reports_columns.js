90ec16e36e888e9683263b9c4b587070ad6e4e23
Raphaël Huchet <raph@selego.co>
2022-12-12 10:57:19 +0100

    fix(api): do not re-delete report columns

--- api/src/db/migrations/2022-11-29_add_reports_columns.js
+++ api/src/db/migrations/2022-11-29_add_reports_columns.js
@@ -3,13 +3,16 @@ const sequelize = require("../sequelize");
 
 module.exports = async () => {
   try {
-    // No injection here, no need to escape.
-    await sequelize.query(`
-      ALTER TABLE "mano"."Report"
-      ADD COLUMN IF NOT EXISTS "team" uuid,
-      ADD COLUMN IF NOT EXISTS "date" text,
-      ADD CONSTRAINT "Report_team_fkey" FOREIGN KEY ("team") REFERENCES "mano"."Team"("_id") ON DELETE CASCADE ON UPDATE CASCADE
+    // Check if constraint Report_team_fkey exists.
+    const [results] = await sequelize.query(`SELECT EXISTS (SELECT 1 FROM   pg_constraint WHERE  conname = 'Report_team_fkey');`);
+    if (!results[0].exists) {
+      await sequelize.query(`
+        ALTER TABLE "mano"."Report"
+        ADD COLUMN IF NOT EXISTS "team" uuid,
+        ADD COLUMN IF NOT EXISTS "date" text,
+        ADD CONSTRAINT "Report_team_fkey" FOREIGN KEY ("team") REFERENCES "mano"."Team"("_id") ON DELETE CASCADE ON UPDATE CASCADE
     `);
+    }
   } catch (e) {
     capture(e);
   }
