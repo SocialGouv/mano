c62981dd898db83cef16c5e6ebbb6778606c11d2
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-12-15 16:26:48 +0100

    fix: clear cache dashboard

--- dashboard/src/services/dataManagement.js
+++ dashboard/src/services/dataManagement.js
@@ -2,7 +2,7 @@ import localforage from 'localforage';
 import { capture } from './sentry';
 
 export let manoCacheStorage = undefined;
-export const dashboardCurrentCacheKey = 'mano_last_refresh_2022_12_01';
+export const dashboardCurrentCacheKey = 'mano_last_refresh_2022_12_15';
 // init
 export async function getManoCacheStorage() {
   if (manoCacheStorage !== undefined) return manoCacheStorage;
