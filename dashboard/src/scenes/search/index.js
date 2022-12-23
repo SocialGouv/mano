dd501e97416605f8e2cd74169e7daf621ba04bf0
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2022-12-23 09:37:09 +0100

    feat(dashboard): sort all persons (#1111)
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>

--- dashboard/src/scenes/search/index.js
+++ dashboard/src/scenes/search/index.js
@@ -15,7 +15,7 @@ import Search from '../../components/search';
 import TagTeam from '../../components/TagTeam';
 import { organisationState, teamsState } from '../../recoil/auth';
 import { actionsState, CANCEL, DONE, sortActionsOrConsultations } from '../../recoil/actions';
-import { personsState } from '../../recoil/persons';
+import { personsState, sortPersons } from '../../recoil/persons';
 import { relsPersonPlaceState } from '../../recoil/relPersonPlace';
 import { sortTerritories, territoriesState } from '../../recoil/territory';
 import { selector, selectorFamily, useRecoilValue } from 'recoil';
@@ -230,12 +230,14 @@ const personsWithFormattedBirthDateSelector = selector({
 const personsFilteredBySearchForSearchSelector = selectorFamily({
   key: 'personsFilteredBySearchForSearchSelector',
   get:
-    ({ search }) =>
+    ({ search, sortBy, sortOrder }) =>
     ({ get }) => {
       const persons = get(personsWithFormattedBirthDateSelector);
       const personsPopulated = get(itemsGroupedByPersonSelector);
       if (!search?.length) return [];
-      return filterBySearch(search, persons).map((p) => personsPopulated[p._id]);
+      return filterBySearch(search, persons)
+        .map((p) => personsPopulated[p._id])
+        .sort(sortPersons(sortBy, sortOrder));
     },
 });
 
@@ -244,7 +246,9 @@ const Persons = ({ search, onUpdateResults }) => {
   const teams = useRecoilValue(teamsState);
   const organisation = useRecoilValue(organisationState);
 
-  const data = useRecoilValue(personsFilteredBySearchForSearchSelector({ search }));
+  const [sortBy, setSortBy] = useLocalStorage('person-sortBy', 'name');
+  const [sortOrder, setSortOrder] = useLocalStorage('person-sortOrder', 'ASC');
+  const data = useRecoilValue(personsFilteredBySearchForSearchSelector({ search, sortBy, sortOrder }));
 
   useEffect(() => {
     onUpdateResults(data.length);
@@ -276,6 +280,10 @@ const Persons = ({ search, onUpdateResults }) => {
             {
               title: '',
               dataKey: 'group',
+              onSortOrder: setSortOrder,
+              onSortBy: setSortBy,
+              sortOrder,
+              sortBy,
               small: true,
               render: (person) => {
                 if (!person.group) return null;
@@ -288,10 +296,21 @@ const Persons = ({ search, onUpdateResults }) => {
                 );
               },
             },
-            { title: 'Nom', dataKey: 'name' },
+            {
+              title: 'Nom',
+              dataKey: 'name',
+              onSortOrder: setSortOrder,
+              onSortBy: setSortBy,
+              sortOrder,
+              sortBy,
+            },
             {
               title: 'Vigilance',
               dataKey: 'alertness',
+              onSortOrder: setSortOrder,
+              onSortBy: setSortBy,
+              sortOrder,
+              sortBy,
               render: (p) =>
                 p.alertness ? (
                   <ExclamationMarkButton
@@ -301,7 +320,15 @@ const Persons = ({ search, onUpdateResults }) => {
                 ) : null,
             },
             { title: 'Équipe(s) en charge', dataKey: 'assignedTeams', render: (person) => <Teams teams={teams} person={person} /> },
-            { title: 'Suivi(e) depuis le', dataKey: 'followedSince', render: (p) => formatDateWithFullMonth(p.followedSince || p.createdAt || '') },
+            {
+              title: 'Suivi(e) depuis le',
+              dataKey: 'followedSince',
+              onSortOrder: setSortOrder,
+              onSortBy: setSortBy,
+              sortOrder,
+              sortBy,
+              render: (p) => formatDateWithFullMonth(p.followedSince || p.createdAt || ''),
+            },
           ].filter((c) => organisation.groupsEnabled || c.dataKey !== 'group')}
         />
       </StyledBox>
