d7bf598c79284e30bdf3424b11956423ef90bf66
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-01-03 16:28:27 +0100

    fix: not constraint but index

--- api/src/db/migrations/2022-12-27_add-unique-constraint-on-report.js
+++ api/src/db/migrations/2022-12-27_add-unique-constraint-on-report.js
@@ -6,7 +6,9 @@ module.exports = async () => {
     // Check if constraint Report_organisation_team_date_key exists.
     const [results] = await sequelize.query(`SELECT EXISTS (SELECT 1 FROM pg_constraint WHERE  conname = 'Report_organisation_team_date_key');`);
     if (!results[0].exists) {
-      await sequelize.query(`alter table "Report" add constraint "Report_organisation_team_date_key" unique (organisation, team, "date");`);
+      await sequelize.query(
+        `create unique index "Report_organisation_team_date_key" on mano."Report" (organisation, "date", team) where date is not NULL and team is not NULL;`
+      );
     }
   } catch (e) {
     capture(e);
