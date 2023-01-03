af85711eafceea945dd3a3012b7ac507387905c3
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-01-03 16:30:24 +0100

    fix: deletedAt not null

--- api/src/db/migrations/2022-12-27_add-unique-constraint-on-report.js
+++ api/src/db/migrations/2022-12-27_add-unique-constraint-on-report.js
@@ -7,7 +7,7 @@ module.exports = async () => {
     const [results] = await sequelize.query(`SELECT EXISTS (SELECT 1 FROM pg_constraint WHERE  conname = 'Report_organisation_team_date_key');`);
     if (!results[0].exists) {
       await sequelize.query(
-        `create unique index "Report_organisation_team_date_key" on mano."Report" (organisation, "date", team) where date is not NULL and team is not NULL;`
+        `create unique index "Report_organisation_team_date_key" on mano."Report" (organisation, "date", team, "deletedAt") where date is not NULL and team is not NULL and "deletedAt" is NULL;`
       );
     }
   } catch (e) {
