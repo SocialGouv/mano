1dc497c58c4c7677c6672bdc3571e05e10e9b6db
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-01-03 16:12:17 +0100

    fix: import migration

--- api/src/db/migrations/index.js
+++ api/src/db/migrations/index.js
@@ -24,4 +24,5 @@
   await require("./2022-11-29_add_grouped_services")();
   await require("./2022-11-29_add_reports_columns")();
   await require("./2022-12-12-debug_report")();
+  await require("./2022-12-27_add-unique-constraint-on-report")();
 })();
