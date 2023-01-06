4d107486c8f63dfc1dc3afd709e83519f098138d
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2023-01-06 14:24:10 +0100

    fix(dashboard): update action teams bug (#1163)
    * fix(dashboard): update action teams bug
    
    * clean

--- dashboard/src/scenes/action/view.js
+++ dashboard/src/scenes/action/view.js
@@ -125,6 +125,7 @@ const ActionView = () => {
         initialValues={action}
         enableReinitialize
         onSubmit={async (body) => {
+          body.teams = Array.isArray(body.teams) ? body.teams : [body.team];
           if (!body.teams?.length) return toast.error('Une action doit être associée à au moins une équipe.');
           const statusChanged = body.status && action.status !== body.status;
           if (statusChanged) {
