0d6e7c0d6f8071f684dfe490fcfd559850bcf307
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-01-10 16:37:11 +0100

    fix: typo

--- dashboard/src/scenes/stats/Reception.js
+++ dashboard/src/scenes/stats/Reception.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { CustomResponsivePie } from './charts';
 import { Block } from './Blocks';
-import { organisationState, teamsState } from '../../recoil/auth';
+import { organisationState } from '../../recoil/auth';
 import { useRecoilValue } from 'recoil';
 import useApi from '../../services/api';
 import { toast } from 'react-toastify';
 import { dayjsInstance } from '../../services/date';
-import { Spinner } from 'reactstrap';
 
 const ReceptionStats = ({ passages, reportsServices, period, teamsId }) => {
   const organisation = useRecoilValue(organisationState);
-  const [servicesFormDatabase, setServicesFormDatabase] = useState(null);
+  const [servicesFromDatabase, setServicesFromDatabase] = useState(null);
   const API = useApi();
   const startDate = useMemo(() => dayjsInstance(period.startDate).format('YYYY-MM-DD'), [period.startDate]);
   const endDate = useMemo(() => dayjsInstance(period.endDate).format('YYYY-MM-DD'), [period.endDate]);
@@ -19,7 +18,7 @@ const ReceptionStats = ({ passages, reportsServices, period, teamsId }) => {
     function fetchServicesStats() {
       API.get({ path: `/service/team/${teamsId.join(',')}/from/${startDate}/to/${endDate}` }).then((res) => {
         if (!res.ok) return toast.error("Erreur lors du chargement des statistiques des services de l'accueil");
-        setServicesFormDatabase(res.data.reduce((acc, service) => ({ ...acc, [service.service]: Number(service.count) }), {}));
+        setServicesFromDatabase(res.data.reduce((acc, service) => ({ ...acc, [service.service]: Number(service.count) }), {}));
       });
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -32,14 +31,14 @@ const ReceptionStats = ({ passages, reportsServices, period, teamsId }) => {
       <div className="-tw-mx-4 tw-flex tw-flex-wrap">
         <Block data={passages.length} title="Nombre de passages" />
       </div>
-      {servicesFormDatabase !== null && (
+      {servicesFromDatabase !== null && (
         <CustomResponsivePie
           title="Services"
           data={organisation.services?.map((service) => {
             return {
               id: service,
               label: service,
-              value: reportsServices.reduce((serviceNumber, rep) => (rep?.[service] || 0) + serviceNumber, 0) + (servicesFormDatabase[service] || 0),
+              value: reportsServices.reduce((serviceNumber, rep) => (rep?.[service] || 0) + serviceNumber, 0) + (servicesFromDatabase[service] || 0),
             };
           })}
         />
