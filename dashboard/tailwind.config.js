a78ee6fa4baf15e18b213c81b57ffd921e54d798
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2023-01-11 17:18:17 +0100

    feat(dashboard): uncheck urgent stuff easily (#1162)

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+    'tw-min-w-0',
+  ],
   theme: {
     extend: {
       colors: {
