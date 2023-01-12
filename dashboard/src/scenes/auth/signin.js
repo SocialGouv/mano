00f5733ad14087125877a53db397ff837efa64e6
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-01-12 14:12:43 +0100

    chore: clean mess in team selection

--- dashboard/src/scenes/auth/signin.js
+++ dashboard/src/scenes/auth/signin.js
@@ -163,7 +163,7 @@ const SignIn = () => {
       <div className="tw-mx-10 tw-my-20 tw-w-full tw-max-w-lg tw-overflow-y-auto tw-overflow-x-hidden tw-rounded-lg tw-bg-white tw-px-7 tw-pt-10 tw-pb-2 tw-text-black tw-shadow-[0_0_20px_0_rgba(0,0,0,0.2)]">
         <h1 className="tw-mb-6 tw-text-center tw-text-3xl tw-font-bold">Choisir son équipe pour commencer</h1>
         <div className="tw-flex tw-w-full tw-flex-col tw-items-center tw-gap-7 [&_>_*]:!tw-w-full">
-          {[...user.teams, ...user.teams, ...user.teams, ...user.teams].map((team) => (
+          {user.teams.map((team) => (
             <ButtonCustom
               key={team._id}
               title={team.name}
