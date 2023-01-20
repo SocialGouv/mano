749ecef31289b790313a666628fcc468e373abb9
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-01-20 15:59:54 +0100

    fix: upgrade 0.71.1

--- app/ios/mano/AppDelegate.mm
+++ app/ios/mano/AppDelegate.mm
@@ -10,6 +10,10 @@
 
   self.moduleName = @"mano";
 
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  // self.initialProps = @{};
+
   [RNBootSplash initWithStoryboard:@"BootSplash" rootView:self.window.rootViewController.view]; // <- initialization using the storyboard file name
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
