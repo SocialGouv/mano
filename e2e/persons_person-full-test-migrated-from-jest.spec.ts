a452e5c40566ea720141ce495c72d83e9ace03eb
Raphaël Huchet <raph@selego.co>
2023-01-23 14:46:28 +0100

    chore: up

--- e2e/persons_person-full-test-migrated-from-jest.spec.ts
+++ e2e/persons_person-full-test-migrated-from-jest.spec.ts
@@ -68,7 +68,6 @@ test("test", async ({ page }) => {
   await page.locator("#react-select-healthInsurances-option-1").click();
   await page.locator(".person-custom-select-couvertures-médicales__value-container").click();
   await page.locator("#react-select-healthInsurances-option-3").click();
-  await page.locator("div:nth-child(3) > .row > div:nth-child(2)").dblclick();
   await page.getByLabel("Structure de suivi médical").fill("dedede");
   await clickOnEmptyReactSelect(page, "person-custom-select-consommations", "Amphétamine/MDMA/Ecstasy");
   await clickOnEmptyReactSelect(page, "person-custom-select-vulnérabilités", "Injecteur");
