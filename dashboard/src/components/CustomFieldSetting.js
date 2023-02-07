45c32887dba5a191d9b5de087c6b6dcd1cc423fb
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-02-07 10:47:35 +0100

    fix: visible par

--- dashboard/src/components/CustomFieldSetting.js
+++ dashboard/src/components/CustomFieldSetting.js
@@ -4,8 +4,7 @@ import { useRecoilValue } from 'recoil';
 import { teamsState } from '../recoil/auth';
 
 const CustomFieldSetting = ({ customField }) => {
-  const { type, label, options = [], enabledTeams, enabled } = customField;
-  const teams = useRecoilValue(teamsState);
+  const { type, label, options = [] } = customField;
   return (
     <div className="tw-flex tw-flex-col tw-items-start tw-justify-start" id={label} data-test-id={label}>
       <p className="tw-m-0 tw-break-keep">{label}</p>
@@ -14,12 +13,25 @@ const CustomFieldSetting = ({ customField }) => {
         {!options?.length ? '' : `: ${options.join(', ')}`}
       </p>
       <p className="tw-m-0 tw-break-keep tw-pl-8 tw-text-xs  tw-opacity-50">
-        {!enabled && enabledTeams?.length
-          ? `Visible par ${enabledTeams.map((teamId) => teams.find((_team) => _team._id === teamId)?.name).join(', ')}`
-          : 'Visible par tous'}
+        <VisibleBy customField={customField} />
       </p>
     </div>
   );
+};
+
+const VisibleBy = ({ customField }) => {
+  const { enabledTeams, enabled } = customField;
+  const teams = useRecoilValue(teamsState);
+  if (!enabled && !enabledTeams?.length) {
+    return <>Non activé</>;
+  }
+  if (enabled || enabledTeams?.length === teams.length) {
+    return <>Visible par tous</>;
+  }
+  if (!enabled && enabledTeams?.length) {
+    return <>Visible par {enabledTeams.map((teamId) => teams.find((_team) => _team._id === teamId)?.name).join(', ')}</>;
+  }
+  return 'Cas non pris';
 };
 
 export default CustomFieldSetting;
