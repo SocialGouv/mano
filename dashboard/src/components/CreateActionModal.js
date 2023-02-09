cd31c2bf30179e3a2d3b8ffd7faaadf6f474c798
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2023-02-09 11:38:45 +0100

    Update dashboard/src/components/CreateActionModal.js
    Co-authored-by: Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>

--- dashboard/src/components/CreateActionModal.js
+++ dashboard/src/components/CreateActionModal.js
@@ -33,10 +33,7 @@ const CreateActionModal = ({ person = null, persons = null, isMulti = false, com
 
   const onAddAction = async (body) => {
     if (body.status !== TODO) body.completedAt = body.completedAt || Date.now();
-    const response = await API.post({
-      path: '/action',
-      body: prepareActionForEncryption({ ...body, user: body.user || user._id }),
-    });
+const response = await API.post({ path: '/action', body: prepareActionForEncryption(body) });
     if (response.ok) setActions((actions) => [response.decryptedData, ...actions]);
     const { createdAt } = response.decryptedData;
     await createReportAtDateIfNotExist(createdAt);
