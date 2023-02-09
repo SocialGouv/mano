ca71ced50ed02d7c0fe81fdd99562cd8cd1b4850
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2023-02-09 11:27:31 +0100

    fix(dashboard): on ne vérifie plus les champs obligatoires lors d'un changement de clé - le but d'un changement de clé est de 'brouiller les pistes', pas de vérifier que tous les champs sont là - on ne garde la vérification que pour l'usage quotidien (#1310)
    * fix(dashboard): on ne vérifie plus les champs obligatoires lors d'un changement de clé - le but d'un changement de clé est de 'brouiller les pistes', pas de vérifier que tous les champs sont là - on ne garde la vérification que pour l'usage quotidien
    
    * fix(dashboard): fix sur #1310 pour ne pas faire le check personnes, groupes, actions, consultations non plus (#1315)
    
    * fix(dashboard): fix sur #1310 pour ne pas faire le check personnes, groupes, actions, consultations non plus
    
    * fix: fix medical file
    
    * fix: fix observation
    
    ---------
    
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>

--- dashboard/src/components/EncryptionKey.js
+++ dashboard/src/components/EncryptionKey.js
@@ -101,23 +101,51 @@ const EncryptionKey = ({ isMain }) => {
       const hashedOrgEncryptionKey = await setOrgEncryptionKey(values.encryptionKey.trim());
       setEncryptingStatus('Chiffrement des données...');
       const encryptedVerificationKey = await encryptVerificationKey(hashedOrgEncryptionKey);
-      const encryptedPersons = await Promise.all(persons.map(preparePersonForEncryption).map(encryptItem));
-      const encryptedGroups = await Promise.all(groups.map(prepareGroupForEncryption).map(encryptItem));
-
-      const encryptedActions = await Promise.all(actions.map(prepareActionForEncryption).map(encryptItem));
-      const encryptedConsultations = await Promise.all(
-        consultations.map(prepareConsultationForEncryption(organisation.consultations)).map(encryptItem)
+      const encryptedPersons = await Promise.all(
+        persons.map((person) => preparePersonForEncryption(person, { checkRequiredFields: false })).map(encryptItem)
       );
-      const encryptedTreatments = await Promise.all(treatments.map(prepareTreatmentForEncryption).map(encryptItem));
-      const encryptedMedicalFiles = await Promise.all(medicalFiles.map(prepareMedicalFileForEncryption(customFieldsMedicalFile)).map(encryptItem));
-      const encryptedComments = await Promise.all(comments.map(prepareCommentForEncryption).map(encryptItem));
-      const encryptedPassages = await Promise.all(passages.map(preparePassageForEncryption).map(encryptItem));
-      const encryptedRencontres = await Promise.all(rencontres.map(prepareRencontreForEncryption).map(encryptItem));
-      const encryptedTerritories = await Promise.all(territories.map(prepareTerritoryForEncryption).map(encryptItem));
-      const encryptedTerritoryObservations = await Promise.all(observations.map(prepareObsForEncryption(customFieldsObs)).map(encryptItem));
-      const encryptedPlaces = await Promise.all(places.map(preparePlaceForEncryption).map(encryptItem));
-      const encryptedRelsPersonPlace = await Promise.all(relsPersonPlace.map(prepareRelPersonPlaceForEncryption).map(encryptItem));
-      const encryptedReports = await Promise.all(reports.map(prepareReportForEncryption).map(encryptItem));
+      const encryptedGroups = await Promise.all(
+        groups.map((group) => prepareGroupForEncryption(group, { checkRequiredFields: false })).map(encryptItem)
+      );
+
+      const encryptedActions = await Promise.all(
+        actions.map((action) => prepareActionForEncryption(action, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedConsultations = await Promise.all(
+        consultations.map(prepareConsultationForEncryption(organisation.consultations, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedTreatments = await Promise.all(
+        treatments.map((treatment) => prepareTreatmentForEncryption(treatment, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedMedicalFiles = await Promise.all(
+        medicalFiles
+          .map((medicalFile) => prepareMedicalFileForEncryption(customFieldsMedicalFile)(medicalFile, { checkRequiredFields: false }))
+          .map(encryptItem)
+      );
+      const encryptedComments = await Promise.all(
+        comments.map((comment) => prepareCommentForEncryption(comment, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedPassages = await Promise.all(
+        passages.map((passage) => preparePassageForEncryption(passage, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedRencontres = await Promise.all(
+        rencontres.map((rencontre) => prepareRencontreForEncryption(rencontre, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedTerritories = await Promise.all(
+        territories.map((territorie) => prepareTerritoryForEncryption(territorie, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedTerritoryObservations = await Promise.all(
+        observations.map((observation) => prepareObsForEncryption(customFieldsObs)(observation, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedPlaces = await Promise.all(
+        places.map((place) => preparePlaceForEncryption(place, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedRelsPersonPlace = await Promise.all(
+        relsPersonPlace.map((relsPersonPlac) => prepareRelPersonPlaceForEncryption(relsPersonPlac, { checkRequiredFields: false })).map(encryptItem)
+      );
+      const encryptedReports = await Promise.all(
+        reports.map((report) => prepareReportForEncryption(report, { checkRequiredFields: false })).map(encryptItem)
+      );
 
       setEncryptingStatus(
         'Sauvegarde des données nouvellement chiffrées en base de donnée. Ne fermez pas votre fenêtre, cela peut prendre quelques minutes...'
@@ -277,8 +305,8 @@ const EncryptionKey = ({ isMain }) => {
   if (organisation.encryptionEnabled && !user.healthcareProfessional)
     return (
       <em>
-        Vous ne pouvez pas changer la clé de chiffrement car vous n'êtes pas déclaré·e comme administrateur·trice de type professionel·le de santé. Il est
-        nécessaire d'avoir accès à l'ensemble des données de l'organisation pour pouvoir changer son chiffrement.
+        Vous ne pouvez pas changer la clé de chiffrement car vous n'êtes pas déclaré·e comme administrateur·trice de type professionel·le de santé. Il
+        est nécessaire d'avoir accès à l'ensemble des données de l'organisation pour pouvoir changer son chiffrement.
       </em>
     );
 
