2add28ac962176c012ee9e8be94c4a41e848ce5a
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-02-10 11:13:00 +0100

    fix(dashboard): statistiques des nouvelles personnes passées

--- dashboard/src/scenes/stats/Passages.js
+++ dashboard/src/scenes/stats/Passages.js
@@ -37,7 +37,7 @@ const PassagesStats = ({ passages, personFields, personsInPassagesOfPeriod, pers
           { options: [...personFields.find((f) => f.name === 'gender').options, 'Non précisé'] }
         )}
       />
-      <AgeRangeBar persons={ personsInPassagesOfPeriod } />
+      <AgeRangeBar persons={personsInPassagesOfPeriod} />
     </>
   );
 };
