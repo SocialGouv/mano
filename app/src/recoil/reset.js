3f98b1da4e38522daafe114bd510f34a34f31ff7
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2023-03-02 11:59:10 +0100

    fix(app): reset du cache quand on change d'organisation (#1365)

---
+++ app/src/recoil/reset.js
@@ -0,0 +1,43 @@
+import { useSetRecoilState } from 'recoil';
+import { personsState } from './persons';
+import { territoryObservationsState } from './territoryObservations';
+import { territoriesState } from './territory';
+import { passagesState } from './passages';
+import { rencontresState } from './rencontres';
+import { commentsState } from './comments';
+import { placesState } from './places';
+import { actionsState } from './actions';
+import { reportsState } from './reports';
+import { groupsState } from './groups';
+import { relsPersonPlaceState } from './relPersonPlace';
+
+const usesetAllCachedDataRecoilStates = () => {
+  const setPersons = useSetRecoilState(personsState);
+  const setActions = useSetRecoilState(actionsState);
+  const setPlaces = useSetRecoilState(placesState);
+  const setComments = useSetRecoilState(commentsState);
+  const setPassages = useSetRecoilState(passagesState);
+  const setRencontres = useSetRecoilState(rencontresState);
+  const setTerritories = useSetRecoilState(territoriesState);
+  const setObservations = useSetRecoilState(territoryObservationsState);
+  const setReports = useSetRecoilState(reportsState);
+  const setGroups = useSetRecoilState(groupsState);
+  const setRelPersonPlaces = useSetRecoilState(relsPersonPlaceState);
+
+  const setAll = () => {
+    setPersons([]);
+    setActions([]);
+    setPlaces([]);
+    setComments([]);
+    setPassages([]);
+    setRencontres([]);
+    setTerritories([]);
+    setObservations([]);
+    setReports([]);
+    setGroups([]);
+    setRelPersonPlaces([]);
+  };
+  return setAll;
+};
+
+export default usesetAllCachedDataRecoilStates;
