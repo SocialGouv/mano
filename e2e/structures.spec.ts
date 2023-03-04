6cd721491383d85cd094fe6da68e3f7d1f74bcf7
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-03-04 11:08:35 +0100

    fix: structures test

--- e2e/structures.spec.ts
+++ e2e/structures.spec.ts
@@ -14,9 +14,19 @@ test.beforeAll(async () => {
 
 test("Structures", async ({ page }) => {
   await loginWith(page, "admin1@example.org");
-  await page.getByRole("link", { name: "Structures" }).click();
-  await expect(page).toHaveURL("http://localhost:8090/structure");
+  await page.getByRole("link", { name: "Organisation" }).click();
+  await page.getByRole("button", { name: "Structures" }).click();
+  await page.locator("details[data-group='Toutes mes catégories']").getByPlaceholder("Ajouter une catégorie").fill("infirmerie");
+  await page.locator("details[data-group='Toutes mes catégories']").getByRole("button", { name: "Ajouter" }).click();
+  await page.getByText("Catégorie ajoutée. Veuillez notifier vos équipes pour qu'elles rechargent leur app ou leur dashboard").click();
+  await page.locator("details[data-group='Toutes mes catégories']").getByPlaceholder("Ajouter une catégorie").fill("infirmerie");
+  await page.locator("details[data-group='Toutes mes catégories']").getByRole("button", { name: "Ajouter" }).click();
+  await page.getByText("Cette catégorie existe déjà: Toutes mes catégories > infirmerie").click();
+  await page.locator("details[data-group='Toutes mes catégories']").getByPlaceholder("Ajouter une catégorie").fill("caruud");
+  await page.locator("details[data-group='Toutes mes catégories']").getByRole("button", { name: "Ajouter" }).click();
+  await page.getByText("Catégorie ajoutée. Veuillez notifier vos équipes pour qu'elles rechargent leur app ou leur dashboard").click();
 
+  await page.getByRole("link", { name: "Structures" }).click();
   await page.getByRole("button", { name: "Créer une nouvelle structure" }).click();
   await page.getByLabel("Nom").fill("Structure 1");
   await page.getByLabel("Téléphone").fill("1123456789");
@@ -49,8 +59,7 @@ test("Structures", async ({ page }) => {
   await page.getByLabel("Code postal").fill("75003");
   await page.getByLabel("Description").fill("joli bébé dort");
   await page.locator(".categories__input-container").click();
-  await page.locator("#categories").fill("piscine");
+  await page.locator("#categories").fill("infirmerie");
   await page.locator("#categories").press("Enter");
-  await page.getByRole("button", { name: "Enregistrer" }).click();
   await page.getByText("Structure mise à jour !").click();
 });
