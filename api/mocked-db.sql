d6a5dc2dd249bbd32dc161078966ebee6f3f02bf
Raphaël Huchet <raph@selego.co>
2023-03-06 15:35:11 +0100

    chore(api): remove old migration system

--- api/mocked-db.sql
+++ api/mocked-db.sql
@@ -89,7 +89,8 @@ CREATE TABLE IF NOT EXISTS mano."Organisation" (
     migrations text[] default array['custom-fields-persons-setup'],
     "migrationLastUpdateAt" timestamp with time zone,
     migrating boolean DEFAULT false,
-    "customFieldsMedicalFile" jsonb
+    "customFieldsMedicalFile" jsonb,
+    "territoriesEnabled" boolean DEFAULT true
 );
 
 --
@@ -171,7 +172,12 @@ CREATE TABLE IF NOT EXISTS mano."Report" (
     encrypted text,
     "encryptedEntityKey" text,
     organisation uuid,
-    "deletedAt" timestamp with time zone
+    "team" uuid,
+    "date" text,
+    "debug" jsonb,
+    "deletedAt" timestamp with time zone,
+    CONSTRAINT "Report_team_fkey" FOREIGN KEY ("team") REFERENCES "mano"."Team"("_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    constraint "Report_organisation_team_date_key" unique (organisation, team, "date")
 );
 
 --
@@ -269,8 +275,49 @@ CREATE TABLE IF NOT EXISTS mano."User" (
     "termsAccepted" timestamp with time zone,
     "healthcareProfessional" boolean DEFAULT false,
     "debugApp" jsonb,
-    "debugDashboard" jsonb
+    "debugDashboard" jsonb,
+    "gaveFeedbackEarly2023" boolean
 );
+
+CREATE TABLE IF NOT EXISTS "mano"."Rencontre" (
+    "_id" uuid,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    "organisation" uuid,
+    "encrypted" text,
+    "encryptedEntityKey" text,
+    "deletedAt" timestamp with time zone,
+    PRIMARY KEY ("_id"),
+    CONSTRAINT "Rencontre_organisation_fkey" FOREIGN KEY ("organisation") REFERENCES "mano"."Organisation"("_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "mano"."Group" (
+    "_id" uuid,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    "organisation" uuid,
+    "encrypted" text,
+    "encryptedEntityKey" text,
+    "deletedAt" timestamp with time zone,
+    PRIMARY KEY ("_id"),
+    CONSTRAINT "Group_organisation_fkey" FOREIGN KEY ("organisation") REFERENCES "mano"."Organisation"("_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create table if not exists mano."Service" (
+    _id uuid not null primary key,
+    service text not null,
+    "count" integer not null,
+    "date" date not null,
+    team uuid,
+    organisation uuid,
+    "createdAt" timestamp with time zone not null,
+    "updatedAt" timestamp with time zone not null,
+    "deletedAt" timestamp with time zone,
+    unique (organisation, service, "date", team),
+    foreign key ("team") references "mano"."Team"("_id") on delete cascade on update cascade,
+    foreign key ("organisation") references "mano"."Organisation"("_id") on delete cascade on update cascade
+);
+create index if not exists idx_service_date_team on mano."Service" ("date", team);
 
 --
 -- Data for Name: Action; Type: TABLE DATA; Schema: mano; Owner: user_feat-pen-test-18fpoe
@@ -867,6 +914,57 @@ ALTER TABLE ONLY mano."Treatment"
 ALTER TABLE ONLY mano."User"
     ADD CONSTRAINT "User_organisation_fkey" FOREIGN KEY (organisation) REFERENCES mano."Organisation"(_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE;
 
+
+
+create index if not exists action_deletedat_idx on mano."Action" ("deletedAt");
+create index if not exists action_updatedat_idx on mano."Action" ("updatedAt");
+create index if not exists action_organisation_idx on mano."Action" (organisation);
+create index if not exists comment_deletedat_idx on mano."Comment" ("deletedAt");
+create index if not exists comment_updatedat_idx on mano."Comment" ("updatedAt");
+create index if not exists comment_organisation_idx on mano."Comment" (organisation);
+create index if not exists consulation_deletedat_idx on mano."Consultation" ("deletedAt");
+create index if not exists consulation_updatedat_idx on mano."Consultation" ("updatedAt");
+create index if not exists consulation_organisation_idx on mano."Consultation" (organisation);
+create index if not exists group_deletedat_idx on mano."Group" ("deletedAt");
+create index if not exists group_updatedat_idx on mano."Group" ("updatedAt");
+create index if not exists group_organisation_idx on mano."Group" (organisation);
+create index if not exists medicalfile_deletedat_idx on mano."MedicalFile" ("deletedAt");
+create index if not exists medicalfile_updatedat_idx on mano."MedicalFile" ("updatedAt");
+create index if not exists medicalfile_organisation_idx on mano."MedicalFile" (organisation);
+create index if not exists passage_deletedat_idx on mano."Passage" ("deletedAt");
+create index if not exists passage_updatedat_idx on mano."Passage" ("updatedAt");
+create index if not exists passage_organisation_idx on mano."Passage" (organisation);
+create index if not exists person_deletedat_idx on mano."Person" ("deletedAt");
+create index if not exists person_updatedat_idx on mano."Person" ("updatedAt");
+create index if not exists person_organisation_idx on mano."Person" (organisation);
+create index if not exists place_deletedat_idx on mano."Place" ("deletedAt");
+create index if not exists place_updatedat_idx on mano."Place" ("updatedAt");
+create index if not exists place_organisation_idx on mano."Place" (organisation);
+create index if not exists relpersonplace_deletedat_idx on mano."RelPersonPlace" ("deletedAt");
+create index if not exists relpersonplace_updatedat_idx on mano."RelPersonPlace" ("updatedAt");
+create index if not exists relpersonplace_organisation_idx on mano."RelPersonPlace" (organisation);
+create index if not exists rencontre_deletedat_idx on mano."Rencontre" ("deletedAt");
+create index if not exists rencontre_updatedat_idx on mano."Rencontre" ("updatedAt");
+create index if not exists rencontre_organisation_idx on mano."Rencontre" (organisation);
+create index if not exists report_deletedat_idx on mano."Report" ("deletedAt");
+create index if not exists report_updatedat_idx on mano."Report" ("updatedAt");
+create index if not exists report_organisation_idx on mano."Report" (organisation);
+create index if not exists service_deletedat_idx on mano."Service" ("deletedAt");
+create index if not exists service_updatedat_idx on mano."Service" ("updatedAt");
+create index if not exists service_organisation_idx on mano."Service" (organisation);
+create index if not exists structure_updatedat_idx on mano."Structure" ("updatedAt");
+create index if not exists structure_organisation_idx on mano."Structure" (organisation);
+create index if not exists territory_deletedat_idx on mano."Territory" ("deletedAt");
+create index if not exists territory_updatedat_idx on mano."Territory" ("updatedAt");
+create index if not exists territory_organisation_idx on mano."Territory" (organisation);
+create index if not exists territoryobservation_deletedat_idx on mano."TerritoryObservation" ("deletedAt");
+create index if not exists territoryobservation_updatedat_idx on mano."TerritoryObservation" ("updatedAt");
+create index if not exists territoryobservation_organisation_idx on mano."TerritoryObservation" (organisation);
+create index if not exists treatment_deletedat_idx on mano."Treatment" ("deletedAt");
+create index if not exists treatment_updatedat_idx on mano."Treatment" ("updatedAt");
+create index if not exists treatment_organisation_idx on mano."Treatment" (organisation);
+create index if not exists user_updatedat_idx on mano."User" ("updatedAt");
+create index if not exists user_organisation_idx on mano."User" (organisation);
 
 --
 -- PostgreSQL database dump complete
--- api/src/db/migrations/index.js
+++ api/src/db/migrations/index.js
@@ -1,34 +1,3 @@
 (async () => {
-  await require("./2023-01-24_can-disable-territories")();
-  await require("./2021-08-18_encrypt-relPersonPlace")();
-  await require("./2021-09-07_terms-acceptance")();
-  await require("./2021-09-10_add-maraude-in-report")();
-  await require("./2021-09-10_team-night-session")();
-  await require("./2021-10-01-custom-fields-obs")();
-  await require("./2021-10-22_encrypted-verification-key")();
-  await require("./2021-11-16_observations-as-json")();
-  await require("./2021-11-26-custom-fields-persons")();
-  await require("./2022-02-14-drop-unencrypted-columns")();
-  await require("./2022-02-22-encrypting")();
-  await require("./2022-03-07_add_migrations_in_organisation")();
-  await require("./2022-03-14_add_passage_table")();
-  await require("./2022-03-16-migrating")();
-  await require("./2022-03-17_user_healthcare_professional")();
-  await require("./2022-03-21-debug-app-dashboard-user")();
-  await require("./2022-03-29_add_consultations_columns")();
-  await require("./2022-04-25_soft-delete")();
-  await require("./2022-05-03_add_medical_file_tables")();
-  await require("./2022-06-17-custom-fields-persons-2")();
-  await require("./2022-09-19_add_rencontre_table")();
-  await require("./2022-11-07_add_grouped_categories")();
-  await require("./2022-11-14_add_group_table")();
-  await require("./2022-11-29_add_grouped_services")();
-  await require("./2022-11-29_add_reports_columns")();
-  await require("./2022-12-12-debug_report")();
-  await require("./2022-12-27_add-unique-constraint-on-report")();
-  await require("./2022-12-30_new-table-service")();
-  await require("./2023-01-16_add-index-everywhere")();
-  await require("./2023-01-18-gave-feedback-2023")();
-  await require("./2023-01-19_custom-fields-person")();
-  await require("./2023-02-16_remove-deleted-and-not-recrypted-data")();
+  // Do not use! Will be removed soon...
 })();
