5988441d287846f0ea6ed195eece715778ab5d52
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-03-20 13:37:58 +0100

    fix: clean migration

--- api/src/db/migrations/20230309171328-add-structures-categories.js
+++ api/src/db/migrations/20230309171328-add-structures-categories.js
@@ -9,7 +9,6 @@ module.exports = {
     SELECT * FROM "mano"."Organisation"
   `);
     for (const organisation of organisations) {
-      if (organisation.migrations?.includes("structures-categories-extract")) continue;
       const structures = await queryInterface.sequelize.query(`SELECT * FROM "mano"."Structure"  WHERE "organisation" = :_id`, {
         replacements: {
           _id: organisation._id,
@@ -39,22 +38,11 @@ module.exports = {
         {
           replacements: {
             structuresGroupedCategories: JSON.stringify(groupedCategories),
-            migrations: `{"${[...(organisation.migrations || []), "structures-categories-extract"].join(`","`)}"}`,
             _id: organisation._id,
           },
           type: Sequelize.QueryTypes.UPDATE,
         }
       );
     }
-  },
-
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.sequelize.query("SELECT 1", { type: Sequelize.QueryTypes.SELECT });
   },
 };
