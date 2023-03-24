87205721d9dec590dd0810cff7cf7f6f60d28b41
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-03-24 14:16:23 +0100

    fix: force dashboard update

--- api/src/config.js
+++ api/src/config.js
@@ -17,7 +17,7 @@ const STORAGE_DIRECTORY = process.env.STORAGE_DIRECTORY;
 const X_TIPIMAIL_APIUSER = process.env.X_TIPIMAIL_APIUSER || "";
 const X_TIPIMAIL_APIKEY = process.env.X_TIPIMAIL_APIKEY || "";
 
-const MINIMUM_DASHBOARD_VERSION = "1.253.0";
+const MINIMUM_DASHBOARD_VERSION = "1.261.4";
 
 module.exports = {
   PORT,
