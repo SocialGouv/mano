eaf2483f016e97163c1f72b77599d644c49529e2
Your Name <you@example.com>
2023-04-13 11:55:34 +0200

    fix: error syntax SQL

--- api/src/db/migrations/20230413091518-add-rencontres-passages-enabled-columns.js
+++ api/src/db/migrations/20230413091518-add-rencontres-passages-enabled-columns.js
@@ -5,14 +5,14 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
     ALTER TABLE "mano"."Organisation"
-    ADD COLUMN IF NOT EXISTS "passagesEnabled" boolean DEFAULT true;
+    ADD COLUMN IF NOT EXISTS "passagesEnabled" boolean DEFAULT true,
     ADD COLUMN IF NOT EXISTS "rencontresEnabled" boolean DEFAULT true;
 
   `);
   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Organisation", "passagesEnabled");
-    await queryInterface.removeColumn("Organisation", "rencontresEnabled");
-  },
+  //async down(queryInterface, Sequelize) {
+  //  await queryInterface.removeColumn("Organisation", "passagesEnabled");
+  //  await queryInterface.removeColumn("Organisation", "rencontresEnabled");
+  //},
 };
