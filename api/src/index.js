290e84701324e06b63114c4d0ce655d77a74a58b
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-05-05 16:10:08 +0200

    fix: possibilité de rechiffrer des organisations avec beaucoup de données jusqu'à 1Go

--- api/src/index.js
+++ api/src/index.js
@@ -54,6 +54,10 @@ app.use(versionCheck);
 
 // Pre middleware
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  const limitedSize = req.path.startsWith("/encrypt") ? "1gb" : "50mb";
+  express.json({ limit: limitedSize })(req, res, next);
+});
 app.use(express.json({ limit: "50mb" }));
 app.use(helmet());
 app.use(cookieParser());
