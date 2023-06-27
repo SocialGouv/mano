267d31b4e59cfe8e6577c1df383b8829416a759d
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-06-27 15:31:30 +0200

    fix: test

--- e2e/consultations_test-navigate-to-consultation.spec.ts
+++ e2e/consultations_test-navigate-to-consultation.spec.ts
@@ -148,7 +148,6 @@ test("test", async ({ page }) => {
   );
 
   await page.goBack();
-  await page.goBack();
   await expect(page).toHaveURL(
     /http:\/\/localhost:8090\/report\/[0-9]{4}-[0-9]{2}-[0-9]{2}\?reportsTeam=%5B%22[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}%22%5D/
   );
@@ -165,7 +164,6 @@ test("test", async ({ page }) => {
     /http:\/\/localhost:8090\/person\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\?tab=Dossier\+M%C3%A9dical/
   );
 
-  await page.goBack();
   await page.goBack();
   await expect(page).toHaveURL(
     /http:\/\/localhost:8090\/report\/[0-9]{4}-[0-9]{2}-[0-9]{2}\?reportsTeam=%5B%22[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}%22%5D/
