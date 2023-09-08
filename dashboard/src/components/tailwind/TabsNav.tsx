703d5bea3000a4ece388009231a822c3db63de78
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-09-08 09:40:07 +0200

    fix: nav to ul

--- dashboard/src/components/tailwind/TabsNav.tsx
+++ dashboard/src/components/tailwind/TabsNav.tsx
@@ -9,24 +9,27 @@ type TabsNavProps = {
 
 export default function TabsNav({ tabs, className, onClick, activeTabIndex }: TabsNavProps) {
   return (
-    <nav className={`noprint tw-flex tw-w-full sm:tw-space-x-4 ${className} tw-border-b tw-border-main tw-border-opacity-20`} aria-label="Tabs">
-      {tabs.map((tab, index) => (
-        <button
-          type="button"
-          key={tab}
-          onClick={() => {
-            onClick(tab, index);
-          }}
-          className={[
-            activeTabIndex === index ? 'tw-bg-main/10 tw-text-black' : 'tw-hover:text-gray-700 tw-text-main',
-            'tw-rounded-md tw-px-3 tw-py-2 tw-text-sm tw-font-medium',
-          ]
-            .filter(Boolean)
-            .join(' ')}
-          aria-current={activeTabIndex ? 'page' : undefined}>
-          {tab}
-        </button>
-      ))}
+    <nav className="noprint tw-flex tw-w-full" aria-label="Tabs">
+      <ul className={`tw-flex tw-w-full tw-list-none sm:tw-space-x-4 ${className} tw-border-b tw-border-main tw-border-opacity-20`}>
+        {tabs.map((tab, index) => (
+          <li key={tab}>
+            <button
+              type="button"
+              onClick={() => {
+                onClick(tab, index);
+              }}
+              className={[
+                activeTabIndex === index ? 'tw-bg-main/10 tw-text-black' : 'tw-hover:text-gray-700 tw-text-main',
+                'tw-rounded-md tw-px-3 tw-py-2 tw-text-sm tw-font-medium',
+              ]
+                .filter(Boolean)
+                .join(' ')}
+              aria-current={activeTabIndex ? 'page' : undefined}>
+              {tab}
+            </button>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
