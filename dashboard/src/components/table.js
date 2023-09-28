74fc2dce0bcd718f3d5ae6d5331c923d1c40ab5f
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-09-28 17:43:39 +0200

    feat(dashboard): person list

--- dashboard/src/components/table.js
+++ dashboard/src/components/table.js
@@ -1,9 +1,11 @@
 import React, { useCallback, useEffect, useRef } from 'react';
 import Sortable from 'sortablejs';
+import useMinimumWidth from '../services/useMinimumWidth';
 
 const Table = ({
   columns = [],
   data = [],
+  renderCellSmallDevices,
   rowKey,
   dataTestId = null,
   onRowClick,
@@ -33,6 +35,8 @@ const Table = ({
     }
   }, [onListChange, isSortable, data.length]);
 
+  const isDesktop = useMinimumWidth('sm');
+
   if (!data.length && noData) {
     return (
       <table className={[className, 'table-selego'].join(' ')}>
@@ -51,84 +55,95 @@ const Table = ({
       </table>
     );
   }
-  return (
-    <table className={[className, 'table-selego'].join(' ')}>
-      <thead>
-        {!!title && (
-          <tr>
-            <td tabIndex={0} aria-label={title} className="title" colSpan={columns.length}>
-              {title}
-            </td>
-          </tr>
-        )}
-        <tr>
-          {columns.map((column) => {
-            const { onSortBy, onSortOrder, sortBy, sortOrder, sortableKey, dataKey } = column;
-            const onNameClick = () => {
-              if (sortBy === sortableKey || sortBy === dataKey) {
-                onSortOrder(sortOrder === 'ASC' ? 'DESC' : 'ASC');
-                return;
-              }
-              onSortBy(sortableKey || dataKey);
-            };
-            return (
-              <td
-                className={[column.className || '', !!onSortBy ? 'tw-cursor-pointer' : 'tw-cursor-default'].join(' ')}
-                style={column.style || {}}
-                key={String(dataKey) + String(column.title)}>
-                <button aria-label="Changer l'ordre de tri" type="button" onClick={!!onSortBy ? onNameClick : null}>
-                  {column.title}
-                </button>
-                {column.help && <>{column.help}</>}
-                {!!onSortBy && (sortBy === sortableKey || sortBy === dataKey) && (
-                  <button onClick={!!onSortBy ? onNameClick : null} type="button" aria-label="Changer l'ordre de tri">
-                    {sortOrder === 'ASC' && <span className="tw-mx-4" onClick={() => onSortOrder('DESC')}>{`\u00A0\u2193`}</span>}
-                    {sortOrder === 'DESC' && <span className="tw-mx-4" onClick={() => onSortOrder('ASC')}>{`\u00A0\u2191`}</span>}
-                  </button>
-                )}
+
+  if (isDesktop || !renderCellSmallDevices) {
+    return (
+      <table className={[className, 'table-selego'].join(' ')}>
+        <thead className="tw-hidden sm:tw-table-header-group">
+          {!!title && (
+            <tr>
+              <td tabIndex={0} aria-label={title} className="title" colSpan={columns.length}>
+                {title}
               </td>
-            );
-          })}
-        </tr>
-      </thead>
-      <tbody ref={gridRef}>
-        {data
-          .filter((e) => e)
-          .map((item) => {
-            return (
-              <tr
-                onClick={() => (!rowDisabled(item) && onRowClick ? onRowClick(item) : null)}
-                onKeyUp={(event) => {
-                  if (event.key === 'Enter')
-                    if (!rowDisabled(item) && onRowClick) {
-                      onRowClick(item);
-                    }
-                }}
-                key={item[rowKey] || item._id}
-                data-key={item[rowKey] || item._id}
-                data-test-id={item[dataTestId] || item[rowKey] || item._id}
-                tabIndex={0}
-                className={[
-                  rowDisabled(item)
-                    ? 'tw-cursor-not-allowed'
-                    : isSortable
-                    ? 'tw-cursor-move'
-                    : Boolean(onRowClick)
-                    ? 'tw-cursor-pointer'
-                    : 'tw-cursor-auto',
-                ].join(' ')}
-                style={item.style || {}}>
-                {columns.map((column) => {
-                  return (
-                    <td className={([column.className || ''].join(' '), !!column.small ? 'small' : 'not-small')} key={item[rowKey] + column.dataKey}>
-                      {column.render ? column.render(item) : item[column.dataKey] || nullDisplay}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-      </tbody>
+            </tr>
+          )}
+          <tr>
+            {columns.map((column) => {
+              const { onSortBy, onSortOrder, sortBy, sortOrder, sortableKey, dataKey } = column;
+              const onNameClick = () => {
+                if (sortBy === sortableKey || sortBy === dataKey) {
+                  onSortOrder(sortOrder === 'ASC' ? 'DESC' : 'ASC');
+                  return;
+                }
+                onSortBy(sortableKey || dataKey);
+              };
+              return (
+                <td
+                  className={[column.className || '', !!onSortBy ? 'tw-cursor-pointer' : 'tw-cursor-default'].join(' ')}
+                  style={column.style || {}}
+                  key={String(dataKey) + String(column.title)}>
+                  <button aria-label="Changer l'ordre de tri" type="button" onClick={!!onSortBy ? onNameClick : null}>
+                    {column.title}
+                  </button>
+                  {column.help && <>{column.help}</>}
+                  {!!onSortBy && (sortBy === sortableKey || sortBy === dataKey) && (
+                    <button onClick={!!onSortBy ? onNameClick : null} type="button" aria-label="Changer l'ordre de tri">
+                      {sortOrder === 'ASC' && <span className="tw-mx-4" onClick={() => onSortOrder('DESC')}>{`\u00A0\u2193`}</span>}
+                      {sortOrder === 'DESC' && <span className="tw-mx-4" onClick={() => onSortOrder('ASC')}>{`\u00A0\u2191`}</span>}
+                    </button>
+                  )}
+                </td>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody ref={gridRef}>
+          {data
+            .filter((e) => e)
+            .map((item) => {
+              return (
+                <tr
+                  onClick={() => (!rowDisabled(item) && onRowClick ? onRowClick(item) : null)}
+                  onKeyUp={(event) => {
+                    if (event.key === 'Enter')
+                      if (!rowDisabled(item) && onRowClick) {
+                        onRowClick(item);
+                      }
+                  }}
+                  key={item[rowKey] || item._id}
+                  data-key={item[rowKey] || item._id}
+                  data-test-id={item[dataTestId] || item[rowKey] || item._id}
+                  tabIndex={0}
+                  className={[
+                    rowDisabled(item)
+                      ? 'tw-cursor-not-allowed'
+                      : isSortable
+                      ? 'tw-cursor-move'
+                      : Boolean(onRowClick)
+                      ? 'tw-cursor-pointer'
+                      : 'tw-cursor-auto',
+                  ].join(' ')}
+                  style={item.style || {}}>
+                  {columns.map((column) => {
+                    return (
+                      <td
+                        className={([column.className || ''].join(' '), !!column.small ? 'small' : 'not-small')}
+                        key={item[rowKey] + column.dataKey}>
+                        {column.render ? column.render(item) : item[column.dataKey] || nullDisplay}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    );
+  }
+
+  return (
+    <table>
+      <tbody ref={gridRef}>{data.filter((e) => e).map(renderCellSmallDevices)}</tbody>
     </table>
   );
 };
