3d4ced0e34cd42bf5a237f1ec199b8920d71fd62
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-10-06 16:26:20 +0200

    fix(dashboard): indication sur l'icône Description

--- dashboard/src/components/DescriptionIcon.js
+++ dashboard/src/components/DescriptionIcon.js
@@ -2,7 +2,12 @@ import React from 'react';
 import openNewWindow from '../assets/icons/description-icon.svg';
 
 const DescriptionIcon = () => (
-  <div className="tw-ml-1 tw-inline-block tw-h-4 tw-w-4 tw-bg-contain tw-text-gray-300" style={{ backgroundImage: `url(${openNewWindow})` }} />
+  <div
+    aria-label="Cette action a une description"
+    title="Cette action a une description"
+    className="tw-ml-1 tw-inline-block tw-h-4 tw-w-4 tw-bg-contain tw-text-gray-300"
+    style={{ backgroundImage: `url(${openNewWindow})` }}
+  />
 );
 
 export default DescriptionIcon;
