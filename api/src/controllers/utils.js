3607ffc5edb9703bf32e5e1aeb546a4a180aa4e8
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-10-16 14:51:08 +0200

    fix: update message

--- api/src/controllers/utils.js
+++ api/src/controllers/utils.js
@@ -29,7 +29,8 @@ router.get("/version", async (req, res) => {
       `La nouvelle version ${MOBILE_APP_VERSION} de Mano est disponible !`,
       `Vous avez la version ${req.headers.version} actuellement sur votre téléphone.
 Cette nouvelle version :
-- Fixe un bug qui fait parfois crasher l'application lors de la consultation d'un dossier
+- Fixe un clash entre suppression d'une personne suivie et visualisation de son dossier médical au même moment
+- Met à jour les Conditions Générales d'Utilisation, Chartes et Mentions Légales
 `,
       [
         { text: "Télécharger", link: `https://mano-app.fabrique.social.gouv.fr/download?ts=${Date.now()}` },
