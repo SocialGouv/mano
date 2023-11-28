cdb503f34a6f6bedbd7213a88685ddcfc0d57d3e
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2023-11-28 12:36:37 +0100

    fix: ajout des équipes dans les consultations d'une organisation (#1790)
    * fix: ajout des équipes dans les consultations d'une organisation
    
    * fix: export secrets
    
    * fix: order

--- dashboard/src/components/DataMigrator.js
+++ dashboard/src/components/DataMigrator.js
@@ -10,6 +10,7 @@ import { looseUuidRegex } from '../utils';
 import { prepareCommentForEncryption } from '../recoil/comments';
 import { prepareGroupForEncryption } from '../recoil/groups';
 import { capture } from '../services/sentry';
+import { prepareConsultationForEncryption } from '../recoil/consultations';
 
 const LOADING_TEXT = 'Mise à jour des données de votre organisation…';
 
@@ -55,6 +56,74 @@ export default function useDataMigrator() {
       }
       // End of example of migration.
       */
+
+      if (organisation._id === process.env.REACT_APP_ORG_IF_FOR_MIGRATION && !organisation.migrations?.includes('add-team-to-consultation')) {
+        setLoadingText('Ajout des équipes dans vos consultations');
+        const consultationsRes = await API.get({
+          path: '/consultation',
+          query: { organisation: organisationId, after: 0, withDeleted: false },
+        }).then((res) => res.decryptedData || []);
+
+        const consultationsToUpdate = consultationsRes
+          .filter((c) => !c.teams?.length)
+          .map((consultation) => {
+            // do something
+            switch (consultation.type) {
+              case 'Médecine générale':
+              case 'Odontologie':
+              case 'Ophtalmologie':
+              case 'Gynécologie':
+              case 'Social':
+              case 'Pédiatrie':
+              case 'Pneumologie':
+              case 'Cardiologie':
+              case 'ORL':
+              case 'IDE':
+              case 'Psycho CDS':
+              case 'Accueil / Secrétariat':
+              case 'Rhumatologie':
+              case 'Maladies Infectieuses':
+              case 'Médiateur Covid':
+              case 'Endocrinologie':
+              case 'Radiologie':
+              case 'Psychiatrie':
+              case 'Psychologique':
+              case 'Infirmier':
+                return {
+                  ...consultation,
+                  teams: [process.env.REACT_APP_TEAM_1],
+                };
+              case 'Perm. Médicale Accueil De Jour':
+              case 'EMSP SOINS':
+              case 'EMSP COORDO':
+              case 'EMSP SOCIAL':
+              case 'EMSP PERM':
+              case 'EMSP Psycho':
+                return {
+                  ...consultation,
+                  teams: [process.env.REACT_APP_TEAM_2],
+                };
+              default:
+                capture('Unknown consultation type', { consultation });
+                return consultation;
+            }
+          });
+
+        const encryptedConsultationsToUpdate = await Promise.all(
+          consultationsToUpdate.map(prepareConsultationForEncryption(organisation.consultations)).map(encryptItem)
+        );
+        const response = await API.put({
+          path: `/migration/add-team-to-consultation`,
+          body: { encryptedConsultationsToUpdate },
+          query: { migrationLastUpdateAt },
+        });
+        if (response.ok) {
+          setOrganisation(response.organisation);
+          migrationLastUpdateAt = response.organisation.migrationLastUpdateAt;
+        } else {
+          return false;
+        }
+      }
 
       return true;
     },
