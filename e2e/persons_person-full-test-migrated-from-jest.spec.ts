ece8d2508c409d6b0d09a0f51820c697b95aa3da
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-12-18 16:00:44 +0100

    fix: test

--- e2e/persons_person-full-test-migrated-from-jest.spec.ts
+++ e2e/persons_person-full-test-migrated-from-jest.spec.ts
@@ -190,7 +190,7 @@ test("test", async ({ page }) => {
   await page.getByRole("button", { name: "Réintégrer dans la file active" }).click();
   await page.getByText(personName + " est réintégré dans la file active").click();
   await page.getByRole("button", { name: "Sortie de file active" }).click();
-  await page.locator('label:has-text("Veuillez préciser le(s) motif(s) de sortie Choisir...") svg').click();
+  await page.locator('label:has-text("Veuillez préciser le(s) motif(s) de sortieChoisir...") svg').click();
   await page.locator("#react-select-outOfActiveListReasons-option-4").click();
   await page.getByRole("button", { name: "Sauvegarder" }).click();
   await page.locator(".alert-warning").getByText("Départ vers autre région").click();
