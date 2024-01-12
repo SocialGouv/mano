77e91bc06eff89a432eba8d97b34a48e9ebac190
Raphaël Huchet <raph@selego.co>
2024-01-12 13:49:50 +0100

    fix: couleur de la pagination

--- dashboard/src/components/pagination.tsx
+++ dashboard/src/components/pagination.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
 import Pagination from 'react-js-pagination';
 import { Col, Row } from 'reactstrap';
 
-const Page = ({ page = 0, limit = 20, total = 0, onChange = () => null }) => (
+const Page = ({ page = 0, limit = 20, total = 0, onChange = ({ page }: { page: number }) => {} }) => (
   <Row>
     <Col md={12} style={{ marginTop: 10, marginBottom: 10 }}>
       <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '1rem' }}>
         <Pagination
           itemClass="page-item"
           linkClass="page-link"
-          activePage={parseInt(page) + 1}
-          itemsCountPerPage={parseInt(limit)}
-          totalItemsCount={parseInt(total)}
+          activePage={page + 1}
+          itemsCountPerPage={limit}
+          totalItemsCount={total}
           pageRangeDisplayed={5}
-          onChange={(page) => onChange({ limit, total, page: page - 1 })}
+          onChange={(page: number) => onChange({ page: page - 1 })}
         />
         <i style={{ marginLeft: '1rem', marginBottom: '1rem', opacity: 0.5 }}>(Total: {total})</i>
       </div>
