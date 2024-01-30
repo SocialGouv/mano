2197a7dfac772ea7ee67b336b3f45de23bb8cb97
Raphaël Huchet <raph@selego.co>
2024-01-30 16:22:04 +0100

    Un autre déploiement

--- website/pages/secret.js
+++ website/pages/secret.js
@@ -2,7 +2,7 @@ export default function Secret() {
   return (
     <div>
       <h1>Secret Page</h1>
-      <p>Shhh, this is a secret page! ET HOP si ça déploie en continu c'est fou. Marches ? Toujours pas</p>
+      <p>Shhh, this is a secret page! ET HOP ?</p>
     </div>
   );
 }
