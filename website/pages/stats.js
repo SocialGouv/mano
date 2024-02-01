a2875b81ad5c6b11eccd476dc78de7e1d9f56244
Raphaël Huchet <raph@selego.co>
2024-02-01 10:50:28 +0100

    Oups

--- website/pages/stats.js
+++ website/pages/stats.js
@@ -5,10 +5,10 @@ const Stats = () => {
   let src = "https://metabase-mano.fabrique.social.gouv.fr/public/dashboard/825cc4ad-b502-4483-aa78-c2af56032bc9";
   if (typeof window !== "undefined") {
     if (window.location.hostname === "preprod-mano.sesan.fr") {
-      url = "http://preprod-metabase-mano.sesan.fr/public/dashboard/d30b0c39-6e14-4bbb-9e64-a367a6fdb073";
+      src = "http://preprod-metabase-mano.sesan.fr/public/dashboard/d30b0c39-6e14-4bbb-9e64-a367a6fdb073";
     }
     if (window.location.hostname === "mano.sesan.fr") {
-      url = "TODO";
+      src = "TODO";
     }
   }
   return (
