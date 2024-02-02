785263b97032e9806d7baf2d0fb745c814e89d8f
Raphaël Huchet <raph@selego.co>
2024-02-02 09:08:25 +0100

    up github actions

--- website/pages/secret.js
+++ website/pages/secret.js
@@ -2,7 +2,7 @@ export default function Secret() {
   return (
     <div>
       <h1>Secret Page</h1>
-      <p>Shhh, this is a secret page! ET HOP ? SI seulement</p>
+      <p>Shhh, this is a secret page! ET HOP ? SI seulement. Je fais des modifs</p>
     </div>
   );
 }
