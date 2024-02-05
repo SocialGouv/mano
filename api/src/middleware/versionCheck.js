e8513c416e82d6195f50e6fa64d07fb7d35021f4
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-02-05 10:38:52 +0100

    fix: on rend l'application mano inaccessible

--- api/src/middleware/versionCheck.js
+++ api/src/middleware/versionCheck.js
@@ -1,39 +1,39 @@
-const { VERSION, MINIMUM_DASHBOARD_VERSION } = require("../config");
+const { QueryTypes } = require("sequelize");
+const { sequelize } = require("../db/sequelize");
 
-const MINIMUM_MOBILE_APP_VERSION = [2, 37, 0];
+let deploymentCommit = null;
+let deploymentDate = null;
 
-module.exports = ({ headers: { version, platform } }, res, next) => {
+module.exports = async ({ headers: { version, platform } }, res, next) => {
   if (platform === "website") return next();
   if (platform === "dashboard") {
-    // Add header with API version to compare with client.
-    res.header("X-API-VERSION", VERSION);
-    res.header("X-MINIMUM-DASHBOARD-VERSION", MINIMUM_DASHBOARD_VERSION);
+    if (deploymentCommit === null) {
+      try {
+        const [deployment] = await sequelize.query(`select commit, "createdAt" from mano."Deployment" order by "createdAt" desc limit 1`, {
+          type: QueryTypes.SELECT,
+        });
+        if (deployment) {
+          deploymentCommit = deployment.commit;
+          deploymentDate = deployment.createdAt.toISOString();
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    console.log(deploymentDate, typeof deploymentDate);
+    res.header("X-API-DEPLOYMENT-COMMIT", deploymentCommit);
+    res.header("X-API-DEPLOYMENT-DATE", deploymentDate);
     // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Expose-Headers
-    res.header("Access-Control-Expose-Headers", "X-API-VERSION, X-MINIMUM-DASHBOARD-VERSION");
+    res.header("Access-Control-Expose-Headers", "X-API-DEPLOYMENT-COMMIT, X-API-DEPLOYMENT-DATE");
     return next();
   }
 
-  // now platform is react native app
-  if (!version) return res.status(403).send({ ok: false, message: "Veuillez mettre à jour votre application!" });
-
-  const appVer = version.split(".").map((d) => parseInt(d));
-
-  for (let i = 0; i < 3; i++) {
-    if (appVer[i] > MINIMUM_MOBILE_APP_VERSION[i]) {
-      return next();
-    } else if (appVer[i] < MINIMUM_MOBILE_APP_VERSION[i]) {
-      return res.status(403).send({
-        ok: false,
-        message: "Veuillez mettre à jour votre application!",
-        inAppMessage: [
-          `Veuillez mettre à jour votre application\u00A0!`,
-          `Les fonctionnalités de cette nouvelle version sont\u00A0:
-- Compatibilité de l 'historique des actions, consultations, traitements et dossier médical (seulement consultable sur navigateur)`,
-          [{ text: "Télécharger la dernière version", link: `https://mano-app.fabrique.social.gouv.fr/download?ts=${Date.now()}` }],
-        ],
-      });
-    }
-  }
-
-  next();
+  // On doit forcer les utilisateurs à télécharger à nouveau Mano
+  return res.status(403).send({
+    ok: false,
+    message: "Veuillez mettre à jour votre application!",
+    inAppMessage: [
+      `Mano a quitté la fabrique pour rejoindre le groupement d'intérêt public SESAN. Pour continuer à utiliser Mano, vous devez mettre à jour votre application mobile.`,
+    ],
+  });
 };
