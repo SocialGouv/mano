0bb0e13f55f181b747fc8c6e9033dd4142802a4c
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-02-16 09:51:19 +0100

    fix: date de fermeture de l'ancien compte rendu

--- dashboard/src/scenes/report/list.js
+++ dashboard/src/scenes/report/list.js
@@ -71,7 +71,7 @@ const List = () => {
   return (
     <>
       <div className="tw-mb-8 tw-border-l-4 tw-border-red-500 tw-bg-red-100 tw-p-4 tw-text-red-700" role="alert">
-        Cet affichage des comptes rendus sera supprimé à partir du 15 février 2024. Utilisez l'onglet "Comptes rendus BETA"
+        Cet affichage des comptes rendus sera supprimé à partir du 19 février 2024. Utilisez l'onglet "Comptes rendus BETA"
       </div>
       <HeaderStyled style={{ padding: '16px 0' }}>
         <div style={{ display: 'flex', flexGrow: '1' }}>
