335e52cfd854d7fd30873d0825bb8d0e7f0bc3fb
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-02-20 11:07:17 +0100

    fix: ajout d'un message de migration sur mobile

--- api/src/controllers/utils.js
+++ api/src/controllers/utils.js
@@ -19,23 +19,21 @@ router.get("/now", passport.authenticate("user", { session: false }), async (req
 // so the mobile app can send a notification to the user.
 // See: app/src/scenes/Login/Login.js
 router.get("/version", async (req, res) => {
-  if (req.headers.version === MOBILE_APP_VERSION) {
-    return res.status(200).send({ ok: true });
-  }
   res.status(200).send({
     ok: false,
     data: MOBILE_APP_VERSION,
     inAppMessage: [
-      `La nouvelle version ${MOBILE_APP_VERSION} de Mano est disponible !`,
-      `Vous avez la version ${req.headers.version} actuellement sur votre téléphone.
-Cette nouvelle version :
-- Corrige l'affichage des rencontres dans les comptes-rendus
-- Corrige le tri des commentaires lorsqu'un nouveau commentaire est ajouté
-`,
-      [
-        { text: "Télécharger", link: `https://mano-app.fabrique.social.gouv.fr/download?ts=${Date.now()}` },
-        { text: "Plus tard", style: "cancel" },
-      ],
+      `Mano ne sera pas accessible le 5 mars dès 9h.`,
+      `Comme vous le savez peut-être déjà, Mano et toute l'équipe quittent la Fabrique numérique des ministères sociaux pour rejoindre le groupement d'intérêt public Sesan (https://www.sesan.fr) et ainsi pérenniser notre action au long-cours !
+Concrètement, pas de changements pour vous :
+- Mano restera chiffré de bout en bout pour protéger les données des personnes que vous accompagnez et leurs données seront toujours stockés sur des serveurs agréés données de santé (certification HDS) situés en France
+- Nous continuerons à vous accompagner
+- Nous continuerons à développer Mano en fonction de vos besoins
+Mano sera cependant indisponible la journée du 5 mars 2024 dès 9h, le temps de réaliser la bascule technique de Mano.
+L'application sera quand à elle indisponible dès le 4 Mars. Vous pourrez de nouveau l'utiliser le 5 Mars en fin de journée.
+Nous restons bien entendu joignables pour répondre à toutes vos questions.
+Toute l'équipe Mano`,
+      [{ text: "Continuer", style: "cancel" }],
       { cancelable: true },
     ],
   });
