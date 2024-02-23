e118f9bd2323a2b0511f0c7094d28383f2637e7f
Raphaël Huchet <raph@selego.co>
2024-02-23 09:12:10 +0100

    Test de modification

--- website/pages/secret.js
+++ website/pages/secret.js
@@ -2,7 +2,7 @@ export default function Secret() {
   return (
     <div>
       <h1>Secret Page</h1>
-      <p>Shhh, this is a secret page! ET HOP ? Nouveau déploiement</p>
+      <p>Shhh, this is a secret page! ET HOP ? Nouveau déploiement, test de modif.</p>
     </div>
   );
 }
