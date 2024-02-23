97313d557f52a14bab355b835da2c77440ed5637
Raphaël Huchet <raph@selego.co>
2024-02-23 08:50:19 +0100

    Amélioration du script de déploiement pour éviter les erreurs de doubles déploiements

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+# Chemin du fichier indicateur de déploiement
+DEPLOY_FILE="/var/deploy/deploy-in-progress.txt"
+
 # Vérifie si un déploiement est déjà en cours
-if pgrep -x "deploy.sh" > /dev/null; then
+if [ -f "$DEPLOY_FILE" ]; then
     echo "Un déploiement est déjà en cours."
     exit 1
 fi
+
+# Crée le fichier indicateur de déploiement
+touch "$DEPLOY_FILE"
 
 # Ajoute les proxy dans l'environnement
 export no_proxy=ad.sesan.fr,.gcsidf.local,localhost,127.0.0.1
@@ -19,3 +27,6 @@ docker compose build website
 docker rollout dashboard
 docker rollout api
 docker rollout website
+
+# Supprime le fichier indicateur de déploiement
+rm "$DEPLOY_FILE"
