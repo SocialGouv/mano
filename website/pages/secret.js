4c8e1841bf56c7ea667f00b58af9f2bb71c1b507
Raphaël Huchet <raph@selego.co>
2024-03-05 15:50:52 +0100

    Déploiement sans prune

--- website/pages/secret.js
+++ website/pages/secret.js
@@ -2,7 +2,7 @@ export default function Secret() {
   return (
     <div>
       <h1>Secret Page</h1>
-      <p>Shhh, this is a secret page! ET HOP ? Nouveau déploiement, test de modif. Juste pour test</p>
+      <p>Shhh, this is a secret page! ET HOP ? Nouveau déploiement, test de modif. Juste pour test. Et maintenant sans prune</p>
     </div>
   );
 }
