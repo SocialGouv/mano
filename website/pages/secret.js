9c18a44a74d086c502bbc922c0e739f9aa9004cc
Raphaël Huchet <raph@selego.co>
2024-03-05 15:43:46 +0100

    Juste un test parce que j'ai lancé prune et que je n'aurai pas du écouter ce conseil

--- website/pages/secret.js
+++ website/pages/secret.js
@@ -2,7 +2,7 @@ export default function Secret() {
   return (
     <div>
       <h1>Secret Page</h1>
-      <p>Shhh, this is a secret page! ET HOP ? Nouveau déploiement, test de modif.</p>
+      <p>Shhh, this is a secret page! ET HOP ? Nouveau déploiement, test de modif. Juste pour test</p>
     </div>
   );
 }
