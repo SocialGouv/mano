8e413494a85a0a9a76a9a8cbdfac3280456cc5bd
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-03-15 14:50:54 +0100

    Mise à jour de sentry pour vite avec sentry wizard (#73)

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,12 +1,22 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "mano-20",
+      project: "mano-espace",
+      telemetry: false,
+      disable: !process.env.CI,
+    }),
+  ],
   build: {
     outDir: "build",
+    sourcemap: true,
   },
   define: {
     "process.env": process.env,
