f7dc0eaecbc6ed5b55761d8e34a71d5ca6f781da
Raphaël Huchet <raph@selego.co>
2024-03-15 10:16:38 +0100

    Correction des icones svg avec vite (back)

--- dashboard/src/components/backButton.jsx
+++ dashboard/src/components/backButton.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import styled from 'styled-components';
-import { theme } from '../config';
-import backImage from '../assets/back.svg';
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../config";
+import backImage from "../assets/back.svg";
 
 const BackButtonWrapperStyled = styled.a`
-  ${(props) => props.withArrow && `background: url(${backImage}) left center no-repeat;`}
+  ${(props) => props.withArrow && `background: url("${backImage}") left center no-repeat;`}
 
   background-size: 20px;
   color: ${theme.main};
@@ -26,7 +25,7 @@ const BackButton = () => {
   return <BackButtonWrapper withArrow onClick={() => history.goBack()} caption="Retour" />;
 };
 
-export const BackButtonWrapper = ({ onClick, caption, withArrow, disabled = false, title = '' }) => (
+export const BackButtonWrapper = ({ onClick, caption, withArrow, disabled = false, title = "" }) => (
   <BackButtonWrapperStyled className="noprint" onClick={onClick} withArrow={withArrow} disabled={disabled} title={title}>
     {caption}
   </BackButtonWrapperStyled>
