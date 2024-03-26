5a37cd7c59ab1398643759e50b9017f674089f13
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2024-03-26 11:57:31 +0100

    chore: overlay for typescript (#85)

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,6 +1,7 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
       telemetry: false,
       disable: !process.env.CI,
     }),
+    !process.env.VITEST ? checker({ typescript: true }) : undefined,
   ],
   build: {
     outDir: "build",
