8c5afcfe622841e748324e877ea2b441752548bd
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-05-20 09:23:01 +0200

    chore: remove styled component back-button (#227)

--- dashboard/src/components/backButton.jsx
+++ dashboard/src/components/backButton.jsx
@@ -1,34 +1,26 @@
 import { useHistory } from "react-router-dom";
-import styled from "styled-components";
-import { theme } from "../config";
-import backImage from "../assets/back.svg";
 
-const BackButtonWrapperStyled = styled.a`
-  ${(props) => props.withArrow && `background: url("${backImage}") left center no-repeat;`}
+const BackButtonWrapper = ({ onClick, caption }) => {
+  return (
+    <a
+      className={`tw-cursor-pointer tw-flex tw-items-center noprint tw-text-main tw-text-sm tw-font-semibold tw-my-4 tw-leading-3 tw-rounded`}
+      onClick={onClick}
+    >
+      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M6.66497 8L9.48766 10.8227C9.75699 11.092 9.75699 11.5287 9.48766 11.798C9.21833 12.0673 8.78167 12.0673 8.51234 11.798L5.202 8.48766C4.93267 8.21833 4.93267 7.78167 5.202 7.51234L8.51234 4.202C8.78167 3.93267 9.21833 3.93267 9.48766 4.202C9.75699 4.47132 9.75699 4.90799 9.48766 5.17732L6.66497 8Z"
+          fill="currentColor"
+        />
+      </svg>
 
-  background-size: 20px;
-  color: ${theme.main};
-  display: inline-block;
-  font-size: 14px;
-  font-weight: 600;
-  padding-left: 25px;
-  margin: 20px 0;
-  line-height: 3;
-  border-radius: 8px;
-  cursor: pointer;
-  border: 0;
-  ${(props) => props.disabled && `opacity: 0.5; pointer-events: none; cursor: default;`}
-`;
+      {caption}
+    </a>
+  );
+};
 
 const BackButton = () => {
   const history = useHistory();
-  return <BackButtonWrapper withArrow onClick={() => history.goBack()} caption="Retour" />;
+  return <BackButtonWrapper onClick={() => history.goBack()} caption="Retour" />;
 };
-
-export const BackButtonWrapper = ({ onClick, caption, withArrow, disabled = false, title = "" }) => (
-  <BackButtonWrapperStyled className="noprint" onClick={onClick} withArrow={withArrow} disabled={disabled} title={title}>
-    {caption}
-  </BackButtonWrapperStyled>
-);
 
 export default BackButton;
