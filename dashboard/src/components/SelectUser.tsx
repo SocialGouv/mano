7c95e74deef9ce8639e2dbbbe770466d907718a0
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2024-06-11 10:37:19 +0200

    fix: le sélecteur d'utilisateurs affiche par défaut l'email quand le nom n'existe pas encore (#280)

--- dashboard/src/components/SelectUser.tsx
+++ dashboard/src/components/SelectUser.tsx
@@ -7,7 +7,15 @@ const SelectUser = ({ value, onChange, ...props }) => {
   const [search, setSearch] = useState("");
   const allUsers = useRecoilValue(usersState);
 
-  const users = search?.length ? allUsers.filter((p) => p.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : allUsers;
+  const users = search?.length
+    ? allUsers
+        .filter((i) => i.name)
+        .filter((p) => {
+          if (p.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) return true;
+          if (p.email.toLocaleLowerCase().includes(search.toLocaleLowerCase())) return true;
+          return false;
+        })
+    : allUsers.filter((i) => i.name);
 
   return (
     <SelectCustom
@@ -17,13 +25,13 @@ const SelectUser = ({ value, onChange, ...props }) => {
       onInputChange={setSearch}
       isSearchable
       onChange={(v) => {
-        setSearch(v?._name);
+        setSearch(v?.name);
         onChange(v?._id);
       }}
       value={users.filter((i) => i._id === value)[0]}
       placeholder={" -- Choisir un utilisateur -- "}
       getOptionValue={(i) => i._id}
-      getOptionLabel={(i) => i?.name || ""}
+      getOptionLabel={(i) => i?.name}
       {...props}
     />
   );
