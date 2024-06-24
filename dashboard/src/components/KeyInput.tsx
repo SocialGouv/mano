1220f59dbf6c7ae31a9654d1543b86e792c8ace6
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2024-06-24 11:49:57 +0200

    feat: oeil sur le champ de la clé de chiffrement pour pouvoir la voir quand on la renseigne (#361)

--- dashboard/src/components/KeyInput.tsx
+++ dashboard/src/components/KeyInput.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { DEFAULT_ORGANISATION_KEY } from "../config";
+import EyeIcon from "../assets/icons/EyeIcon";
 
 // Un faux champ password pour la clé de chiffrement.
 //
@@ -17,6 +18,7 @@ const KeyInput = ({
 }) => {
   const inputRef = useRef<HTMLDivElement>(null);
   const [value, setValue] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
   useEffect(() => {
     if (DEFAULT_ORGANISATION_KEY) {
@@ -35,92 +37,111 @@ const KeyInput = ({
   }
 
   return (
-    <div
-      id={id}
-      contentEditable
-      ref={inputRef}
-      onPaste={(e) => {
-        // Pas de copier/coller
-        e.preventDefault();
-      }}
-      onKeyDown={(e) => {
-        // 1. On ne veut pas rajouter de caractère "entrée"
-        // 2. On envoie la clé
-        if (e.key === "Enter") {
+    <div className="tw-relative tw-mb-1.5 tw-block tw-w-full tw-rounded tw-border tw-border-main75 tw-bg-transparent tw-outline-main tw-transition-all">
+      <div
+        id={id}
+        contentEditable
+        ref={inputRef}
+        onPaste={(e) => {
+          // Pas de copier/coller
           e.preventDefault();
-          onPressEnter(e);
-        }
-      }}
-      onDoubleClick={(e) => {
-        // Gestion de la sélection de tout le texte (car il est en contradiction avec les onSelect)
-        e.preventDefault();
-        e.stopPropagation();
-        document.getSelection().selectAllChildren(inputRef.current);
-      }}
-      onSelect={(e) => {
-        e.preventDefault();
-        // Toute sélection est désactivée, sauf si on a sélectionné tout le texte
-        if (document.getSelection().toString().length !== inputRef.current.innerText.length) {
+        }}
+        onKeyDown={(e) => {
+          // 1. On ne veut pas rajouter de caractère "entrée"
+          // 2. On envoie la clé
+          if (e.key === "Enter") {
+            e.preventDefault();
+            onPressEnter(e);
+          }
+        }}
+        onDoubleClick={(e) => {
+          // Gestion de la sélection de tout le texte (car il est en contradiction avec les onSelect)
+          e.preventDefault();
+          e.stopPropagation();
+          document.getSelection().selectAllChildren(inputRef.current);
+        }}
+        onSelect={(e) => {
+          e.preventDefault();
+          // Toute sélection est désactivée, sauf si on a sélectionné tout le texte
+          if (document.getSelection().toString().length !== inputRef.current.innerText.length) {
+            setCursorToEnd();
+          }
+        }}
+        onCompositionEnd={(e) => {
+          // Gestion des caractères en 2 temps (similaire à l'ajout d'un caractère à la fin).
+          const innerText = (e.target as HTMLElement).innerText;
+          const newValue = value + innerText.slice(-1);
+          onChange(newValue);
+          setValue(newValue);
+          if (!showPassword) {
+            (e.target as HTMLElement).innerText = "•".repeat(newValue.length);
+          }
           setCursorToEnd();
-        }
-      }}
-      onCompositionEnd={(e) => {
-        // Gestion des caractères en 2 temps (similaire à l'ajout d'un caractère à la fin).
-        const innerText = (e.target as HTMLElement).innerText;
-        const newValue = value + innerText.slice(-1);
-        onChange(newValue);
-        setValue(newValue);
-        (e.target as HTMLElement).innerText = "•".repeat(newValue.length);
-        setCursorToEnd();
-      }}
-      onInput={(e) => {
-        // Cas particulier: Si on est en train de faire une composition (comme ^ + i pour î par exemple), on gère à l'extérieur.
-        // Sauf que sur android, on ne peut pas détecter la composition parce que tout semble composition, donc on laisse passer.
-        if (!isAndroid() && (e.nativeEvent as InputEvent).isComposing) return;
+        }}
+        onInput={(e) => {
+          // Cas particulier: Si on est en train de faire une composition (comme ^ + i pour î par exemple), on gère à l'extérieur.
+          // Sauf que sur android, on ne peut pas détecter la composition parce que tout semble composition, donc on laisse passer.
+          if (!isAndroid() && (e.nativeEvent as InputEvent).isComposing) return;
 
-        let innerText = (e.target as HTMLElement).innerText;
-        // On enlève les caractères de retour à la ligne (sur iOS, quand on efface tout, il reste un retour à la ligne)
-        innerText = (innerText || "").replace(/[\r\n]/g, "");
-        let newValue: string;
+          let innerText = (e.target as HTMLElement).innerText;
+          // On enlève les caractères de retour à la ligne (sur iOS, quand on efface tout, il reste un retour à la ligne)
+          innerText = (innerText || "").replace(/[\r\n]/g, "");
+          let newValue: string;
 
-        if (innerText.length === value.length + 1) {
-          // 1 caractère en plus à la fin
-          const newChar = innerText.slice(-1);
-          newValue = value + newChar;
-        } else if (innerText.length + 1 === value.length) {
-          // 1 caractère en moins à la fin
-          newValue = value.slice(0, -1);
-        } else {
-          // Autre cas
-          newValue = innerText;
-        }
+          if (innerText.length === value.length + 1) {
+            // 1 caractère en plus à la fin
+            const newChar = innerText.slice(-1);
+            newValue = value + newChar;
+          } else if (innerText.length + 1 === value.length) {
+            // 1 caractère en moins à la fin
+            newValue = value.slice(0, -1);
+          } else {
+            // Autre cas
+            newValue = innerText;
+          }
 
-        // Cas particulier en cas d'erreur (si la personne a forcé le déplacement du curseur par exemple)
-        if (newValue.includes("•")) {
-          newValue = "";
-        }
+          // Cas particulier en cas d'erreur (si la personne a forcé le déplacement du curseur par exemple)
+          if (newValue.includes("•")) {
+            newValue = "";
+          }
 
-        // Replace non-breaking spaces by normal spaces
-        // eslint-disable-next-line no-irregular-whitespace
-        newValue = newValue.replace(/ /g, " ");
+          // Replace non-breaking spaces by normal spaces
+          // eslint-disable-next-line no-irregular-whitespace
+          newValue = newValue.replace(/ /g, " ");
 
-        // State interne et envoi de la clé
-        onChange(newValue);
-        setValue(newValue);
+          // State interne et envoi de la clé
+          onChange(newValue);
+          setValue(newValue);
 
-        // Remplacement du texte par des points
-        (e.target as HTMLElement).innerText = "•".repeat(newValue.length);
+          // Remplacement du texte par des points
+          if (!showPassword) {
+            (e.target as HTMLElement).innerText = "•".repeat(newValue.length);
+          }
 
-        // On déplace le curseur à la fin
-        setCursorToEnd();
-      }}
-      autoCorrect="off"
-      spellCheck="false"
-      autoCapitalize="off"
-      // Il est très important de laisser `tw-whitespace-pre-wrap` pour éviter
-      // que le navigateur remplace les espaces par des espaces insécables.
-      className="tw-whitespace-pre-wrap tw-mb-1.5 tw-block tw-w-full tw-rounded tw-border tw-border-main75 tw-bg-transparent tw-p-2.5 tw-text-black tw-outline-main tw-transition-all"
-    />
+          // On déplace le curseur à la fin
+          setCursorToEnd();
+        }}
+        autoCorrect="off"
+        spellCheck="false"
+        autoCapitalize="off"
+        // Il est très important de laisser `tw-whitespace-pre-wrap` pour éviter
+        // que le navigateur remplace les espaces par des espaces insécables.
+        className="tw-whitespace-pre-wrap tw-block tw-w-full tw-bg-transparent tw-p-2.5 tw-text-black tw-outline-main tw-transition-all"
+      />
+      <EyeIcon
+        strikedThrough={showPassword}
+        onClick={() => {
+          const nextShowPassword = !showPassword;
+          if (nextShowPassword === false) {
+            inputRef.current.innerText = "•".repeat(value.length);
+          } else {
+            inputRef.current.innerText = value;
+          }
+          setShowPassword(nextShowPassword);
+        }}
+        className="tw-absolute tw-right-4 tw-top-2 tw-cursor-pointer"
+      />
+    </div>
   );
 };
 
