81043a4e119653d80746661d327d2c0f9f0e8d61
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-07-01 12:10:08 +0200

    chore: on réaffiche la capture sentry dans la console, c'était bien pratique pour débuguer (#389)

--- dashboard/src/services/sentry.js
+++ dashboard/src/services/sentry.js
@@ -1,6 +1,7 @@
 import * as Sentry from "@sentry/react";
 
 export const capture = (err, context = {}) => {
+  console.log("capture", err, context);
   if (typeof context === "string") {
     context = JSON.parse(context);
   } else {
