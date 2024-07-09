56a03d11e1d19d87fcf44170007b9904da9ff1af
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-07-09 09:28:18 +0200

    chore: type inutile sur show password (#441)

--- dashboard/src/components/PasswordInput.jsx
+++ dashboard/src/components/PasswordInput.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import EyeIcon from "../assets/icons/EyeIcon";
 
-const PasswordInput = ({ className, showPassword, setShowPassword, type, ...props }) => (
+const PasswordInput = ({ className, showPassword, setShowPassword, ...props }) => (
   <div className="tw-relative tw-mb-1.5 tw-flex tw-cursor-pointer tw-items-center">
     <input
       className={[className, "!tw-mb-0", showPassword ? "show-password" : "hide-password"].join(" ")}
-      type={type || showPassword ? "text" : "password"}
+      type={showPassword ? "text" : "password"}
       autoCapitalize="off"
       {...props}
     />
